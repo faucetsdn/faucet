7a1bf20346e69a4b29f8eda47d2f80f3f99081df
Yu Wang <Winston@NetworkSeer.com>
2016-04-05 02:12:34 +1000

    lu

--- src/ryu_faucet/org/onfsdn/faucet/valve.py
+++ src/ryu_faucet/org/onfsdn/faucet/valve.py
@@ -898,9 +898,13 @@ class OVSStatelessValve(Valve):
         datapath = msg.datapath
         self.logger.info("before match")
         in_port = msg.match['in_port']
+        self.logger.info("1")
         src_ip = msg.match['ipv4_src']
+        self.logger.info("2")
         dst_ip = msg.match['ipv4_dst']
+        self.logger.info("3")
         src_port = msg.match['tcp_src']
+        self.logger.info("4")
         dst_port = msg.match['tcp_dst']
         self.logger.info("before ofpmatch")
         match = parser.OFPMatch(in_port= in_port, ipv4_src = src_ip, ipv4_dst = dst_ip, tcp_src = src_port, tcp_dst = dst_port)
