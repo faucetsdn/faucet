b44558d00382bb338ffe5a5ac024bb66a8b99d08
Himal Kumar <himal.2708@gmail.com>
2016-04-05 00:20:41 +1000

    hg

--- src/ryu_faucet/org/onfsdn/faucet/faucet.py
+++ src/ryu_faucet/org/onfsdn/faucet/faucet.py
@@ -168,14 +168,12 @@ class Faucet(app_manager.RyuApp):
         else:
             return
         
-        in_port = msg.match['in_port']
-        flowmods = self.valve.rcv_packet(dp.id, in_port, vlan_vid, msg.match, pkt)
-        self.send_flow_msgs(dp, flowmods)
+ 
         
 
         self.logger.info("before ip_hdr")
         ip_hdr = pkt.get_protocols(ipv4.ipv4)
-        
+
         if len(ip_hdr)!=0:
             src_ip = ip_hdr[0].src
             dst_ip = ip_hdr[0].dst
@@ -190,7 +188,14 @@ class Faucet(app_manager.RyuApp):
                     self.logger.info("inserting this particular flow entry: %s:%s %s:%s", src_ip,src_port,dst_ip,dst_port)
                     flowmods = self.valve.netflix_flows_insertion(ev) 
                     dp.send_msg(flowmods)
-                    self.logger.info("this also done wooohoooooo")       
+                    self.logger.info("this also done wooohoooooo")
+                    return  
+
+        in_port = msg.match['in_port']
+        flowmods = self.valve.rcv_packet(dp.id, in_port, vlan_vid, msg.match, pkt)
+        self.send_flow_msgs(dp, flowmods)     
+
+
 
         # if ip_src in netflix_src_list :
         #     src_ip = msg.match['src_ip']
