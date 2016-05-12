326d401c261d81288d8801dd652f9ddb5a11c4ed
shivarammysore <shivaram.mysore@gmail.com>
2016-05-12 10:45:36 -0700

    Revert "More fixes to make it work with Python3.5"
    This reverts commit c7cce631e7b64e7c80ece98b7915d3a88e8ee7b7.

--- src/ryu_faucet/org/onfsdn/faucet/util.py
+++ src/ryu_faucet/org/onfsdn/faucet/util.py
@@ -22,18 +22,18 @@ def dump(obj, level=0):
     if type(obj) == dict:
         for k, v in obj.items():
             if hasattr(v, '__iter__'):
-                print ("%s%s" % (prefix, k))
+                print "%s%s" % (prefix, k)
                 dump(v, level+1)
             else:
-                print ("%s%s : %s" % (prefix, k, v))
+                print "%s%s : %s" % (prefix, k, v)
     elif type(obj) == list:
         for v in obj:
             if hasattr(v, '__iter__'):
                 dump(v, level+1)
             else:
-                print ("%s%s" % (prefix, v))
+                print "%s%s" % (prefix, v)
     else:
-        print ("%s%s" % (prefix, obj))
+        print "%s%s" % (prefix, obj)
 
 def mac_addr_is_unicast(mac_addr):
     """Returns True if mac_addr is a unicast ethernet address.
