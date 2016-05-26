72bdca9e135a2cad099aae2c00cdebff70eb2f14
Simeon Miteff <simeon.miteff@gmail.com>
2016-05-26 16:18:06 +0200

    Don't need this (wrong for setup.py develop)

--- docker/runtests.sh
+++ docker/runtests.sh
@@ -8,5 +8,4 @@ echo "=========== Running faucet unit tests ==========="
 time python -m unittest -v faucet_mininet_test
 
 echo "========== Running faucet config tests =========="
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/dist-packages/ryu_faucet/org/onfsdn/faucet/
 python test_config.py
