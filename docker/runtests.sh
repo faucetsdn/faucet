d3d41f4399cb8cf4c8c9c80941bdd2a524b40c99
Josh Bailey <joshb@google.com>
2016-08-30 04:46:29 +0000

    https://github.com/inside-openflow/faucet/commit/f46cb0777119d4609d65b806dbae96ee8abab353 - fix docker test run to run in parallel and install missing dependency on pylint version.

--- docker/runtests.sh
+++ docker/runtests.sh
@@ -5,7 +5,7 @@ service openvswitch-switch start
 cd /faucet-src/tests
 
 echo "=========== Running faucet unit tests ==========="
-time python -m unittest -v faucet_mininet_test
+time ./faucet_mininet_test.py
 
 echo "========== Running faucet config tests =========="
-python test_config.py
+./test_config.py
