66bfca8a4fed638fcef48fcb551660c0d0913d68
Josh Bailey <joshb@google.com>
2016-09-30 06:44:23 +0000

    Log DPID consistently in faucet.py.

--- src/ryu_faucet/org/onfsdn/faucet/faucet.py
+++ src/ryu_faucet/org/onfsdn/faucet/faucet.py
@@ -24,7 +24,7 @@ import ipaddr
 
 from config_parser import dp_parser
 from valve import valve_factory
-from util import kill_on_exception, get_sys_prefix, get_logger
+from util import kill_on_exception, get_sys_prefix, get_logger, dpid_log
 
 from ryu.base import app_manager
 from ryu.controller.handler import CONFIG_DISPATCHER
@@ -103,7 +103,8 @@ class Faucet(app_manager.RyuApp):
             # pylint: disable=no-member
             valve = valve_factory(dp)
             if valve is None:
-                self.logger.error('Hardware type not supported for DP: %s', dp.name)
+                self.logger.error(
+                    'Hardware type not supported for DP: %s', dp.name)
             else:
                 self.valves[dp.dp_id] = valve(dp, self.logname)
 
@@ -191,7 +192,7 @@ class Faucet(app_manager.RyuApp):
 
     def send_flow_msgs(self, dp, flow_msgs):
         if dp.id not in self.valves:
-            self.logger.error('send_flow_msgs: unknown dp with id %u', dp.id)
+            self.logger.error('send_flow_msgs: unknown %s', dpid_log(dp.id))
             return
         self.valves[dp.id].ofchannel_log(flow_msgs)
         for flow_msg in flow_msgs:
@@ -257,7 +258,7 @@ class Faucet(app_manager.RyuApp):
         valve = self.valves[dp.id] if dp.id in self.valves else None
 
         if not valve:
-            self.logger.error('_packet_in_handler: unknown dp with id: %u', dp.id)
+            self.logger.error('_packet_in_handler: unknown %s', dpid_log(dp.id))
             return
 
         valve.ofchannel_log([msg])
@@ -286,7 +287,7 @@ class Faucet(app_manager.RyuApp):
             self.valves[dp.id].ofchannel_log([msg])
             self.logger.error('Got OFError: %s', msg)
         else:
-            self.logger.error('_error_handler: unknown dp with id: %u', dp.id)
+            self.logger.error('_error_handler: unknown %s', dpid_log(dp.id))
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER) # # pylint: disable=no-member
     def handler_features(self, ev):
@@ -296,7 +297,7 @@ class Faucet(app_manager.RyuApp):
             flowmods = self.valves[dp.id].switch_features(dp.id, msg)
             self.send_flow_msgs(dp, flowmods)
         else:
-            self.logger.error('handler_features: unknown dp with id: %u', dp.id)
+            self.logger.error('handler_features: unknown %s', dpid_log(dp.id))
 
     @set_ev_cls(dpset.EventDP, dpset.DPSET_EV_DISPATCHER)
     @kill_on_exception(exc_logname)
@@ -306,21 +307,21 @@ class Faucet(app_manager.RyuApp):
         if not ev.enter:
             if dp.id in self.valves:
                 # Datapath down message
-                self.logger.debug('DP %u disconnected', dp.id)
+                self.logger.debug('%s disconnected', dpid_log(dp.id))
                 self.valves[dp.id].datapath_disconnect(dp.id)
             else:
                 self.logger.error(
-                    'handler_connect_or_disconnect: unknown dp with id: %u', dp.id)
+                    'handler_connect_or_disconnect: unknown %s', dpid_log(dp.id))
             return
 
-        self.logger.debug('DP %u connected', dp.id)
+        self.logger.debug('%s connected', dpid_log(dp.id))
         self.handler_datapath(dp)
 
     @set_ev_cls(dpset.EventDPReconnected, dpset.DPSET_EV_DISPATCHER)
     @kill_on_exception(exc_logname)
     def handler_reconnect(self, ev):
         dp = ev.dp
-        self.logger.debug('DP %u reconnected', dp.id)
+        self.logger.debug('%s reconnected', dpid_log(dp.id))
         self.handler_datapath(dp)
 
     def handler_datapath(self, dp):
@@ -330,7 +331,7 @@ class Faucet(app_manager.RyuApp):
             flowmods = self.valves[dp.id].datapath_connect(dp.id, discovered_ports)
             self.send_flow_msgs(dp, flowmods)
         else:
-            self.logger.error('handler_datapath: unknown dp with id: %u', dp.id)
+            self.logger.error('handler_datapath: unknown %s', dpid_log(dp.id))
 
     @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER) # pylint: disable=no-member
     @kill_on_exception(exc_logname)
@@ -343,7 +344,7 @@ class Faucet(app_manager.RyuApp):
 
         if dp.id not in self.valves:
             self.logger.error(
-                'port_status_handler: unknown dp with id: %u', dp.id)
+                'port_status_handler: unknown %s', dpid_log(dp.id))
             return
 
         valve = self.valves[dp.id]
