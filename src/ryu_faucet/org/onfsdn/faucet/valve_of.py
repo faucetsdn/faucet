2032365bc1cde4c1dc25a31ef94aeff7f957238a
Josh Bailey <joshb@google.com>
2016-10-02 22:57:33 +0000

    Incremental documentation of valve_of.

--- src/ryu_faucet/org/onfsdn/faucet/valve_of.py
+++ src/ryu_faucet/org/onfsdn/faucet/valve_of.py
@@ -1,3 +1,5 @@
+"""Utility functions to parse/create OpenFlow messages."""
+
 # Copyright (C) 2013 Nippon Telegraph and Telephone Corporation.
 # Copyright (C) 2015 Brad Cowie, Christopher Lorier and Joe Stringer.
 # Copyright (C) 2015 Research and Education Advanced Network New Zealand Ltd.
@@ -23,66 +25,173 @@ from ryu.ofproto import ofproto_v1_3_parser as parser
 
 
 def ignore_port(port_num):
-    """Ignore non-physical ports."""
-    # port numbers > 0xF0000000 indicate a logical port
+    """Return True if FAUCET should ignore this port.
+
+    Args:
+        port_num (int): switch port.
+    Returns:
+        bool: True if FAUCET should ignore this port.
+    """
+    # 0xF0000000 and up are not physical ports.
     return port_num > 0xF0000000
 
+
 def apply_actions(actions):
+    """Return instruction that applies action list.
+
+    Args:
+        actions (list): list of OpenFlow actions.
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPInstruction: instruction of actions.
+    """
     return parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS, actions)
 
+
 def goto_table(table_id):
+    """Return instruction to goto table.
+
+    Args:
+        table_id (int): table to goto.
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPInstruction: goto instruction.
+    """
     return parser.OFPInstructionGotoTable(table_id)
 
+
 def set_eth_src(eth_src):
+    """Return action to set source Ethernet MAC address.
+
+    Args:
+        eth_src (str): source Ethernet MAC address.
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPActionSetField: set field action.
+    """
     return parser.OFPActionSetField(eth_src=eth_src)
 
+
 def set_eth_dst(eth_dst):
+    """Return action to set destination Ethernet MAC address.
+
+    Args:
+        eth_src (str): destination Ethernet MAC address.
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPActionSetField: set field action.
+    """
     return parser.OFPActionSetField(eth_dst=eth_dst)
 
+
 def vid_present(vid):
+    """Return VLAN VID with VID_PRESENT flag set.
+
+    Args:
+        vid (int): VLAN VID
+    Returns:
+        int: VLAN VID with VID_PRESENT.
+    """
     return vid | ofp.OFPVID_PRESENT
 
+
 def push_vlan_act(vlan_vid):
+    """Return OpenFlow action list to push Ethernet 802.1Q header with VLAN VID.
+
+    Args:
+        vid (int): VLAN VID
+    Returns:
+        list: actions to push 802.1Q header with VLAN VID set.
+    """
     return [
         parser.OFPActionPushVlan(ether.ETH_TYPE_8021Q),
         parser.OFPActionSetField(vlan_vid=vid_present(vlan_vid))
     ]
 
+
 def dec_ip_ttl():
+    """Return OpenFlow action to decrement IP TTL.
+
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPActionDecNwTtl: decrement IP TTL.
+    """
     return parser.OFPActionDecNwTtl()
 
+
 def pop_vlan():
+    """Return OpenFlow action to pop outermost Ethernet 802.1Q VLAN header.
+
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPActionPopVlan: Pop VLAN.
+    """
     return parser.OFPActionPopVlan()
 
-def output_port(port_no, max_len=0):
-    return parser.OFPActionOutput(port_no, max_len=max_len)
+
+def output_port(port_num, max_len=0):
+    """Return OpenFlow action to output to a port.
+
+    Args:
+        port_num (int): port to output to.
+        max_len (int): maximum length of packet to output (default no maximum).
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPActionOutput: output to port action.
+    """
+    return parser.OFPActionOutput(port_num, max_len=max_len)
+
 
 def output_controller():
+    """Return OpenFlow action to packet in to the controller (max 256 bytes).
+
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPActionOutput: packet in action.
+    """
     return output_port(ofp.OFPP_CONTROLLER, 256)
 
-def packetout(out_port, data):
+
+def packetout(port_num, data):
+    """Return OpenFlow action to packet out to dataplane from controller.
+
+    Args:
+        port_num (int): port to output to.
+        data (str): raw packet to output.
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPActionOutput: packet out action.
+    """
     return parser.OFPPacketOut(
         datapath=None,
         buffer_id=ofp.OFP_NO_BUFFER,
         in_port=ofp.OFPP_CONTROLLER,
-        actions=[output_port(out_port)],
+        actions=[output_port(port_num)],
         data=data)
 
+
 def barrier():
+    """Return OpenFlow barrier request.
+
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPBarrierRequest: barrier request.
+    """
     return parser.OFPBarrierRequest(None)
+
 
 def table_features(body):
     return parser.OFPTableFeaturesStatsRequest(
         datapath=None, body=body)
 
-def match(match_dict):
-    return parser.OFPMatch(**match_dict)
+
+def match(match):
+    """Return OpenFlow matches from dict.
+
+    Args:
+        match (dict): match fields and values.
+    Returns:
+        ryu.ofproto.ofproto_v1_3_parser.OFPMatch: matches.
+    """
+    return parser.OFPMatch(**match)
+
 
 def match_from_dict(match_dict):
     null_dp = namedtuple('null_dp', 'ofproto_parser')
     null_dp.ofproto_parser = parser
     acl_match = ofctl.to_match(null_dp, match_dict)
     return acl_match
+
 
 def build_match_dict(in_port=None, vlan=None,
                      eth_type=None, eth_src=None,
@@ -124,6 +233,7 @@ def build_match_dict(in_port=None, vlan=None,
     if eth_type is not None:
         match_dict['eth_type'] = eth_type
     return match_dict
+
 
 def flowmod(cookie, command, table_id, priority, out_port, out_group,
             match, inst, hard_timeout, idle_timeout):
