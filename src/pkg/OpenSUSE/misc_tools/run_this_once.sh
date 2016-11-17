29a88978eb5ca15185b1e9ff8ebd9f78e14d9059
shivarammysore <shivaram.mysore@gmail.com>
2016-11-17 08:47:32 -0800

    updating scripts
    code cleanup by removing unused and unnecessary items

--- src/pkg/OpenSUSE/misc_tools/run_this_once.sh
+++ src/pkg/OpenSUSE/misc_tools/run_this_once.sh
@@ -3,11 +3,10 @@
 
 pip install networkx
 pip install ovs ryu
-pip install https://pypi.python.org/packages/77/ba/1bf4547a58dfc41f502c0ad640d282c44d63be26f7d852dae5bc04904a51/ryu-faucet-1.2.tar.gz
+pip install ryu-faucet
 pip show ryu_faucet
 
 echo "Showing all network interfaces that are up ..."
 ip link ls up
 echo "Modify dpid and ports in files:"
 echo "1.  /etc/ryu/faucet/faucet.yaml"
-
