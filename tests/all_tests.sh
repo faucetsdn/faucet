342d0ea3dffc354def4d908292e86e488c4c9275
Josh Bailey <joshb@google.com>
2016-12-05 22:15:42 +1300

    Clear stale logs.

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -1,1 +1,1 @@
-killall ryu-manager ; ./faucet_mininet_test.py -c ; ./faucet_mininet_test.py
+rm -rf /tmp/faucet*log /tmp/gauge*log ; killall ryu-manager ; ./faucet_mininet_test.py -c ; ./faucet_mininet_test.py
