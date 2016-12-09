ea13f83c40606c8906a1f7fd208f70554edbea99
Josh Bailey <joshb@google.com>
2016-12-09 17:08:16 +1300

    Run test_valve and test_config.

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -1,1 +1,1 @@
-rm -rf /tmp/faucet*log /tmp/gauge*log ; killall ryu-manager ; ./faucet_mininet_test.py -c ;/usr/local/share/openvswitch/scripts/ovs-ctl stop ; /usr/local/share/openvswitch/scripts/ovs-ctl start ; ./faucet_mininet_test.py $*
+rm -rf /tmp/faucet*log /tmp/gauge*log ; killall ryu-manager ; ./faucet_mininet_test.py -c ; ./test_config.py && ./test_valve.py && /usr/local/share/openvswitch/scripts/ovs-ctl stop ; /usr/local/share/openvswitch/scripts/ovs-ctl start ; ./faucet_mininet_test.py $*
