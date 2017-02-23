49993e41631aece828f6f4b470dbc3b169d48c78
shivarammysore <shivaram.mysore@gmail.com>
2017-02-23 11:06:49 -0800

    Bump version number to 1.4 - prep for release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 
     setup(
         name='ryu-faucet',
-        version='1.3.2',
+        version='1.4',
         packages=['ryu_faucet'],
         package_dir={'ryu_faucet': 'src/ryu_faucet'},
         ## Temp Fix for v1.2 to not use data_files_prefix as we have package install issues.
@@ -50,7 +50,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
         author='Christopher Lorier',
         author_email='chris.lorier@reannz.co.nz',
         maintainer='Shivaram Mysore, FaucetSDN.Org',
-        maintainer_email='shivaram.mysore@gmail.com, faucet-dev@googlegroups.com',
+        maintainer_email='shivaram.mysore@gmail.com, faucet-dev@list.waikato.ac.nz',
         classifiers=[
             'Development Status :: 5 - Production/Stable',
             'Environment :: Console',
