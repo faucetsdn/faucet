2b1e9037c5eb9d17d41d7212fe53493eb128f9bb
shivarammysore <shivaram.mysore@gmail.com>
2017-02-28 10:15:43 -0800

    Fix typo and added a sample start-faucet script for quick reference

---
+++ src/pkg/Ubuntu/start-faucet.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+## Sample start-faucet script for reference
+/usr/local/bin/ryu-manager --ofp-tcp-listen-port 6653  /usr/local/lib/python2.7/dist-packages/ryu_faucet/org/onfsdn/faucet/faucet.py
