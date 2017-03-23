b20c47d607b709ebc5e7c21ce77db5277c598beb
Christopher Lorier <chris.lorier@reannz.co.nz>
2017-03-23 13:27:57 +1300

    Add Copyright assertion on behalf of Contributors
        Only done in places where REANNZ has copyright.

--- tests/fakeoftable.py
+++ tests/fakeoftable.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2015 Research and Innovation Advanced Network New Zealand Ltd.
+# Copyright (C) 2015--2017 The Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
