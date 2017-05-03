02b4f6a9513aa5213de0573b6a514a7221e7d625
Brad Cowie <brad@wand.net.nz>
2017-05-03 13:39:26 +1200

    Use py2 and py3 compatible io.read() instead of read().

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
+import io
 import os
 import re
 
 from setuptools import setup
 
 setup_dir = os.path.dirname(__file__)
-readme_contents = open(os.path.join(setup_dir, 'README.rst')).read()
+readme_contents = io.open(os.path.join(setup_dir, 'README.rst'), encoding="utf-8").read()
 faucet_version = re.match(r'.+version: ([0-9\.]+)', readme_contents).group(1)
 os.environ["PBR_VERSION"] = faucet_version
 
