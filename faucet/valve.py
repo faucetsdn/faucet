eb794e8ceabe9136d5c4850cdfb947c308ac7dc7
Josh Bailey <joshb@google.com>
2017-06-10 18:51:10 +1200

    pylint.

--- faucet/valve.py
+++ faucet/valve.py
@@ -1349,7 +1349,7 @@ class Valve(object):
 class ArubaValve(Valve):
     """Valve implementation that uses OpenFlow send table features messages."""
 
-    def switch_features(self, _, _):
+    def switch_features(self, dp_id, msg):
         pipeline_config_dir = aruba.CFG_PATH
         if self.dp.pipeline_config_dir is not None:
             pipeline_config_dir = self.dp.pipeline_config_dir
