d209b336147b046bcf321572cc1144d47c1df0cb
Samuel Ribeiro <ribeiro@google.com>
2017-07-19 17:52:10 +0200

    Make the gNMI client run.
    - Create a GOPATH in the home folder;
    - Go get the correct gNMI client code;
    - Add a gNMI tests placeholder in the runtests.sh
    to actually run the client and display help;

--- docker/runtests.sh
+++ docker/runtests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "================= Starting OVS =================="
+echo "========== Running gNMI tests ==================="
+export GOPATH=$HOME/go
+# Just a placeholder that runs a gNMI client and displays help.
+go run $GOPATH/src/github.com/openconfig/reference/telemetry/collector/cli/main.go --help || exit 0
+
+echo "========== Starting OVS ========================="
 service openvswitch-switch start
 
 cd /faucet-src/tests
@@ -8,9 +13,9 @@ echo "========== Running faucet config tests =========="
 python3 ./test_config.py || exit 1
 python3 ./test_check_config.py || exit 1
 
-echo "=========== Running faucet unit tests ==========="
+echo "========== Running faucet unit tests ============"
 python3 ./test_valve.py || exit 1
 
-echo "=========== Running faucet system tests ==========="
+echo "========== Running faucet system tests =========="
 python2 ./faucet_mininet_test.py -c
 time ./faucet_mininet_test.py $FAUCET_TESTS || exit 1
