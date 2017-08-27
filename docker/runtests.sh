990e357bd749fb95940b612eb516206963b769ab
Josh Bailey <joshb@google.com>
2017-08-27 16:59:32 +1200

    Docker tests should skip expensive pytype etc when -n specified.

--- docker/runtests.sh
+++ docker/runtests.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+
+DEPCHECK=1
+
+# if -n passed, don't check dependencies/lint/type.
+while getopts "n" o $FAUCET_TESTS; do
+  case "${o}" in
+        n)
+            DEPCHECK=0
+            ;;
+        *)
+            ;;
+    esac
+done
+
+echo "========== checking IPv4/v6 localhost is up ====="
+ping6 -c 1 ::1 || exit 1
+ping -c 1 127.0.0.1 || exit 1
+
 echo "========== Starting OVS ========================="
 service openvswitch-switch start
-
 ovs-vsctl show || exit 1
 
 # enable fast reuse of ports.
@@ -11,12 +28,14 @@ sysctl -w net.ipv4.tcp_syn_retries=4
 
 cd /faucet-src/tests
 
+if [ "$DEPCHECK" == 1 ] ; then
+    echo "============ Running pytype analyzer ============"
+    # TODO: pytype doesn't completely understand py3 yet.
+    ls -1 ../faucet/*py | parallel --bar pytype -d import-error || exit 1
+fi
+
 echo "========== Running faucet unit tests =========="
 py.test ./test_check_config.py ./test_config.py ./test_valve.py --cov faucet --doctest-modules -v --cov-report term-missing
-
-echo "============ Running pytype analyzer ============"
-# TODO: pytype doesn't completely understand py3 yet.
-ls -1 ../faucet/*py | parallel --bar pytype -d import-error || exit 1
 
 echo "========== Running faucet system tests =========="
 python2 ./faucet_mininet_test.py -c
