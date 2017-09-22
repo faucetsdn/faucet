f247fee90b614db0ea64973bb3008e30e17696cc
Josh Bailey <joshb@google.com>
2017-09-22 18:28:24 +1200

    Experimental fctl utility for FAUCET/Gauge command line use.

---
+++ faucet/fctl.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+"""Standalone script to query FAUCET/Gauge/Prometheus for statistics."""
+
+# Copyright (C) 2015 Brad Cowie, Christopher Lorier and Joe Stringer.
+# Copyright (C) 2015 Research and Education Advanced Network New Zealand Ltd.
+# Copyright (C) 2015--2017 The Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import time
+import getopt
+import requests
+from prometheus_client import parser
+
+
+VAL_DECODE = {
+    'learned_macs': lambda mac: "{:012x}".format(int(mac))
+}
+
+
+def scrape_prometheus(http_endpoints, retries=3):
+    metrics = []
+    for http_endpoint in http_endpoints:
+        content = None
+        for _ in range(retries):
+            try:
+                response = requests.get(http_endpoint)
+                if response.status_code == 200:
+                    content = response.content.decode('utf-8', 'strict')
+                    break
+            except ValueError:
+                time.sleep(1)
+        endpoint_metrics = parser.text_string_to_metric_families(
+            content)
+        metrics.extend(endpoint_metrics)
+    return metrics
+
+
+def report_label_match_metrics(report_metrics, metrics,
+                               nonzero_only=False, delim='\t', label_matches=None):
+    for metric in metrics:
+        if not report_metrics or metric.name in report_metrics:
+            for _, labels, value in metric.samples:
+                if label_matches and set(label_matches.items()).issubset(set(labels.items())):
+                    if nonzero_only and int(value) == 0:
+                        continue
+                    try:
+                        value = VAL_DECODE[metric.name](value)
+                    except KeyError:
+                        pass
+                    print(delim.join((metric.name, str(labels), str(value))))
+
+
+def usage():
+    usage_vars = {'self': sys.argv[0]}
+    print("""
+Retrieve FAUCET/Gauge state using Prometheus.
+
+    {self} [-n] <-e|--endpoints=http://prometheus:port> [-m|--metrics=prometheus_metrics,] [-l|--labels=name:value,]
+
+    -n: Don't report 0 values
+    -e|--endpoints: list of Prometheus endpoints to query (comma separated)
+    -m|--metrics: list of Prometheus variables to query (comma separated)
+    -l|--labels: filter list of Prometheus variables by labels that must be present (comma separated)
+
+Examples:
+
+    MACs learned on a DP.
+
+    {self} -n --endpoints=http://172.17.0.1:9244 --metrics=learned_macs --labels=dp_id:0xb827eb608918
+
+    Status of all DPs
+
+    {self} -n --endpoints=http://172.17.0.1:9244 --metrics=dp_status
+""".format(**usage_vars))
+    sys.exit(-1)
+
+
+def main():
+    try:
+        opts, _ = getopt.getopt(
+            sys.argv[1:], 'ne:m:l:', ['nonzero', 'endpoints=', 'metrics=', 'labels='])
+    except getopt.GetoptError:
+        usage()
+
+    endpoints = []
+    report_metrics = []
+    label_matches = {}
+    nonzero_only = False
+
+    for opt, arg in opts:
+        if opt in ('-n', '--nonzero'):
+            nonzero_only = True
+        elif opt in ('-e', '--endpoints'):
+            endpoints = arg.split(',')
+        elif opt in ('-m', '--metrics'):
+            report_metrics = arg.split(',')
+        elif opt in ('-l', '--labels'):
+            for label_value in arg.split(','):
+                label, value = label_value.split(':')
+                label_matches[label] = value
+        else:
+            usage()
+
+    metrics = scrape_prometheus(endpoints)
+    report_label_match_metrics(
+        report_metrics, metrics, nonzero_only=nonzero_only, label_matches=label_matches)
+
+
+if __name__ == '__main__':
+    main()
