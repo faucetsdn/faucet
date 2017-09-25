c0aa777b56c7a2f6ac3a28a5db58b7484fb7125c
Josh Bailey <joshb@google.com>
2017-09-25 20:36:01 +1300

    FaucetMetrics() has no args.

--- tests/test_valve.py
+++ tests/test_valve.py
@@ -30,7 +30,7 @@ from faucet.valve import valve_factory
 from faucet.config_parser import dp_parser
 from faucet.faucet_metrics import FaucetMetrics
 
-METRICS = FaucetMetrics(0, '127.0.0.1')
+METRICS = FaucetMetrics()
 
 
 def build_pkt(pkt):
