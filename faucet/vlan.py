991e8bc8584263d8b562084a2a86776cafb35db7
Josh Bailey <joshb@google.com>
2017-10-12 06:36:12 +1300

    Always filter flood ports for only up ports.

--- faucet/vlan.py
+++ faucet/vlan.py
@@ -210,10 +210,11 @@ class VLAN(Conf):
         return [port for port in self.get_ports() if port.mirror_destination]
 
     def flood_ports(self, configured_ports, exclude_unicast):
+        up_configured_ports = [port for port in configured_ports if port.running()]
         if exclude_unicast:
-            return [port for port in configured_ports if port.unicast_flood]
+            return [port for port in up_configured_ports if port.unicast_flood]
         else:
-            return configured_ports
+            return up_configured_ports
 
     def tagged_flood_ports(self, exclude_unicast):
         return self.flood_ports(self.tagged, exclude_unicast)
@@ -228,9 +229,8 @@ class VLAN(Conf):
                 (None, self.untagged_flood_ports(False))):
             if ports:
                 pkt = packet_builder(self, vid, *args)
-                for port in ports:
-                    if port.running():
-                        ofmsgs.append(valve_of.packetout(port.number, pkt.data))
+                flood_ofmsgs = [valve_of.packetout(port.number, pkt.data) for port in ports]
+                ofmsgs.extend(flood_ofmsgs)
         return ofmsgs
 
     def port_is_tagged(self, port):
