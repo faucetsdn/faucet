38d087ccc60bf0b7bc1fd17284a3ad08f85b4997
Josh Bailey <joshb@google.com>
2017-10-16 09:00:53 +1300

    Adding missing import.

--- faucet/dp.py
+++ faucet/dp.py
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import collections
 import networkx
 from ryu.ofproto import ofproto_v1_3 as ofp
 
