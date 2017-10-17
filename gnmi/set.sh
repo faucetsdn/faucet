4e90e2458d73b42bcae4dfb2fab3af7b9b182745
Samuel Ribeiro <ribeiro@google.com>
2017-10-17 06:31:19 +0200

    gNMI, use new replace/update methods.

--- gnmi/set.sh
+++ gnmi/set.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 gnmi_set \
+  -replace /system/openflow/agent/config/max-backoff:12 \
+  -update /system/clock/config/timezone-name:"US/New York" \
   -target_addr $GNMI_TARGET:$GNMI_PORT \
   -key $HOME/certs/client.key \
   -cert $HOME/certs/client.crt \
   -ca $HOME/certs/ca.crt \
   -target_name server.com \
-  -alsologtostderr \
-  -json_update target_configs/typical_ofsw_config.json
+  -alsologtostderr
