a4fb653f127f125c3abf79bd5cb27655921a39b7
Josh Bailey <joshb@google.com>
2017-11-20 11:27:42 +1300

    More directory confusion.

--- run-travis-test.sh
+++ run-travis-test.sh
@@ -2,11 +2,11 @@
 
 # TODO: run different tests based on matrix.
 touch ~/.pylintrc
-cd ~/tests
+cd ./tests
 PYTHONPATH=~/faucet ./test_min_pylint.sh
 PYTHONPATH=~/faucet python3 -m pytest ./test_*.py --cov faucet --doctest-modules -v --cov-report term-missing
 coveralls || true
-cd ~/
+cd ..
 docker build -t ${FAUCET_TEST_IMG} -f Dockerfile.tests .
 docker images
 sudo docker run --privileged -t -e FAUCET_TESTS="-d" ${FAUCET_TEST_IMG}
