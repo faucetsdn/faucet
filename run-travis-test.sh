13d3d850061e68394050514444ba0dc2f4d7e83f
Josh Bailey <joshb@google.com>
2017-11-20 15:29:19 +1300

    Revert "Shard on route/non routing tests."
    This reverts commit f0813a08df5515984f124c3100e787162c1ffde9.

--- run-travis-test.sh
+++ run-travis-test.sh
@@ -10,11 +10,8 @@ cd ..
 docker build -t ${FAUCET_TEST_IMG} -f Dockerfile.tests .
 docker images
 
-ALLTESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" tests/faucet_mininet_test_unit.py|cut -f2 -d" "`
-SINGLETESTS=`echo $ALLTESTS|grep -E -o "\bFaucetSingle\S+\b"`
-PARALLELTESTS=`echo $ALLTESTS|grep -v -E -o "\bFaucetSingle\S+\b"`
-ROUTINGTESTS=`echo $PARALLELTESTS|grep -E -o "\bFaucet.+Rout.+\S+\b"`
-NONROUTINGTESTS=`echo $PARALLELTESTS|grep -v -E -o "\bFaucet.+Rout.+\S+\b"`
+SINGLETESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" tests/faucet_mininet_test_unit.py|cut -f2 -d" "|grep FaucetSingle`
+PARALLELTESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" tests/faucet_mininet_test_unit.py|cut -f2 -d" "|grep -v FaucetSingle`
 
 echo single process tests: ${SINGLETESTS}
 echo parallelizable tests: ${PARALLELTESTS}
@@ -23,11 +20,8 @@ case ${TRAVIS_MATRIX} in
   SINGLE)
     RUNTESTS=${SINGLETESTS}
     ;;
-  ROUTING)
-    RUNTESTS=${ROUTINGTESTS}
-    ;;
-  NONROUTING)
-    RUNTESTS=${NONROUTINGTESTS}
+  PARALLEL)
+    RUNTESTS=${PARALLELTESTS}
     ;;
   *)
     RUNTESTS=''
