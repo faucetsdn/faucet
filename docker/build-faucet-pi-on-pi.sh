22ead93635416c318a32cd9f4f14c481529dde4c
Brad Cowie <brad@wand.net.nz>
2017-11-21 09:57:02 +1300

    Tidy up docker pi build scripts.

--- docker/build-faucet-pi-on-pi.sh
+++ docker/build-faucet-pi-on-pi.sh
@@ -2,10 +2,6 @@
 
 # run from cron on Raspberry Pi build farm.
 
-export TMPDIR=/tmp/`basename $0`
-rm -rf $TMPDIR
-mkdir -p $TMPDIR
-
 date
 cd $(dirname $0) && \
 cd .. && \
@@ -13,11 +9,12 @@ git stash && \
 git checkout -q master && \
 git pull 2>&1 || exit 1
 
-export DOCKER_ID_USER="faucet"
-export DOCKER="docker"
-export TAGS=`wget -q -O- 'https://registry.hub.docker.com/v2/repositories/faucet/faucet-pi/tags?page_size=1024'|jq --raw-output '."results"[]["name"]'|grep -E "^[0-9\.]+$"|sort > $TMPDIR/tags.txt`
-export REPOTAGS=`git tag|grep -E "^[0-9\.]+$"|sort > $TMPDIR/repotags.txt`
-export MISSINGTAGS=`diff -u $TMPDIR/tags.txt $TMPDIR/repotags.txt |grep -E "^\+[0-9]+"|sed "s/\+//g"`
+TMPDIR=$(mktemp -d /tmp/$(basename $0).XXXXXX)
+DOCKER_ID_USER="faucet"
+DOCKER="docker"
+TAGS=$(wget -q -O- 'https://registry.hub.docker.com/v2/repositories/faucet/faucet-pi/tags?page_size=1024'|jq --raw-output '."results"[]["name"]'|grep -E "^[0-9\.]+$"|sort > $TMPDIR/tags.txt)
+REPOTAGS=$(git tag|grep -E "^[0-9\.]+$"|sort > $TMPDIR/repotags.txt)
+MISSINGTAGS=$(diff -u $TMPDIR/tags.txt $TMPDIR/repotags.txt |grep -E "^\+[0-9]+"|sed "s/\+//g")
 
 build_tag()
 {
@@ -25,10 +22,8 @@ build_tag()
     branch=$2
     echo "building tag $tag (branch $branch)"
     git checkout -q $branch
-    $DOCKER build -t faucet-pi -f Dockerfile.pi .
-    $DOCKER build -t gauge-pi -f Dockerfile.pi-gauge .
-    $DOCKER tag -f faucet-pi $DOCKER_ID_USER/faucet-pi:$tag
-    $DOCKER tag -f gauge-pi $DOCKER_ID_USER/gauge-pi:$tag
+    $DOCKER build -t $DOCKER_ID_USER/faucet-pi:$tag -f Dockerfile.pi .
+    $DOCKER build -t $DOCKER_ID_USER/gauge-pi:$tag -f Dockerfile.pi-gauge .
     $DOCKER push $DOCKER_ID_USER/faucet-pi:$tag
     $DOCKER push $DOCKER_ID_USER/gauge-pi:$tag
 }
@@ -43,4 +38,4 @@ fi
 build_tag latest master
 
 $DOCKER rmi -f $($DOCKER images --filter "dangling=true" -q --no-trunc) 2>&1
-rm -rf $TMPDIR
+rm -rf "$TMPDIR"
--- docker/build-prometheus-pi-on-pi.sh
+++ docker/build-prometheus-pi-on-pi.sh
@@ -2,10 +2,6 @@
 
 # run from cron on Raspberry Pi build farm.
 
-export TMPDIR=/tmp/`basename $0`
-rm -rf $TMPDIR
-mkdir -p $TMPDIR
-
 date
 cd $(dirname $0) && \
 cd .. && \
@@ -13,8 +9,9 @@ git stash && \
 git checkout -q master && \
 git pull 2>&1 || exit 1
 
-export DOCKER_ID_USER="faucet"
-export DOCKER="docker"
+TMPDIR=$(mktemp -d /tmp/$(basename $0).XXXXXX)
+DOCKER_ID_USER="faucet"
+DOCKER="docker"
 
 build_tag()
 {
@@ -22,13 +19,12 @@ build_tag()
     branch=$2
     echo "building tag $tag (branch $branch)"
     git checkout -q $branch
-    $DOCKER build -t prometheus-pi -f docker/prometheus/Dockerfile .
-    $DOCKER tag -f prometheus-pi $DOCKER_ID_USER/prometheus-pi:$tag
+    $DOCKER build -t $DOCKER_ID_USER/prometheus-pi:$tag -f docker/prometheus/Dockerfile .
     $DOCKER push $DOCKER_ID_USER/prometheus-pi:$tag
 }
 
-VERSION=$(grep -Eo "ARG.*VERSION=[0-9\.]+" docker/prometheus/Dockerfile | cut -d '=' -f 2)
-build_tag $VERSION master
+version=$(grep -Eo "ARG.*VERSION=[0-9\.]+" docker/prometheus/Dockerfile | cut -d '=' -f 2)
+build_tag $version master
 
 $DOCKER rmi -f $($DOCKER images --filter "dangling=true" -q --no-trunc) 2>&1
-rm -rf $TMPDIR
+rm -rf "$TMPDIR"
