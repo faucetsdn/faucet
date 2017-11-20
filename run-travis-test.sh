f0813a08df5515984f124c3100e787162c1ffde9
Josh Bailey <joshb@google.com>
2017-11-20 14:59:09 +1300

    Shard on route/non routing tests.

--- run-travis-test.sh
+++ run-travis-test.sh
@@ -10,8 +10,11 @@ cd ..
 docker build -t ${FAUCET_TEST_IMG} -f Dockerfile.tests .
 docker images
 
-SINGLETESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" tests/faucet_mininet_test_unit.py|cut -f2 -d" "|grep FaucetSingle`
-PARALLELTESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" tests/faucet_mininet_test_unit.py|cut -f2 -d" "|grep -v FaucetSingle`
+ALLTESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" tests/faucet_mininet_test_unit.py|cut -f2 -d" "`
+SINGLETESTS=`echo $ALLTESTS|grep -E -o "\bFaucetSingle\S+\b"`
+PARALLELTESTS=`echo $ALLTESTS|grep -v -E -o "\bFaucetSingle\S+\b"`
+ROUTINGTESTS=`echo $PARALLELTESTS|grep -E -o "\bFaucet.+Rout.+\S+\b"`
+NONROUTINGTESTS=`echo $PARALLELTESTS|grep -v -E -o "\bFaucet.+Rout.+\S+\b"`
 
 echo single process tests: ${SINGLETESTS}
 echo parallelizable tests: ${PARALLELTESTS}
@@ -20,8 +23,11 @@ case ${TRAVIS_MATRIX} in
   SINGLE)
     RUNTESTS=${SINGLETESTS}
     ;;
-  PARALLEL)
-    RUNTESTS=${PARALLELTESTS}
+  ROUTING)
+    RUNTESTS=${ROUTINGTESTS}
+    ;;
+  NONROUTING)
+    RUNTESTS=${NONROUTINGTESTS}
     ;;
   *)
     RUNTESTS=''
