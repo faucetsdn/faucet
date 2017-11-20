5d46db9b69eacb771738cf38c628705ea5cbbf72
Josh Bailey <joshb@google.com>
2017-11-20 22:27:59 +1300

    Simplify test list parsing.

--- run-travis-test.sh
+++ run-travis-test.sh
@@ -10,8 +10,9 @@ cd ..
 docker build -t ${FAUCET_TEST_IMG} -f Dockerfile.tests .
 docker images
 
-SINGLETESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" tests/faucet_mininet_test_unit.py|cut -f2 -d" "|grep FaucetSingle`
-PARALLELTESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" tests/faucet_mininet_test_unit.py|cut -f2 -d" "|grep -v FaucetSingle`
+ALLTESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" tests/faucet_mininet_test_unit.py|cut -f2 -d" "`
+SINGLETESTS=`echo "$ALLTESTS" | grep -o -E "\b(FaucetSingle.+Test)\b"`
+PARALLELTESTS=`echo "$ALLTESTS" | grep -v -E "\b(FaucetSingle.+Test)\b"`
 
 echo single process tests: ${SINGLETESTS}
 echo parallelizable tests: ${PARALLELTESTS}
