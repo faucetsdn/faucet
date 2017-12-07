ee403db15f0b9447f4f11c86fa47d532256b63da
mab68 <mab68@students.waikato.ac.nz>
2017-12-07 16:09:15 +1300

    Optimized config-fuzzer and related files

--- docker/fuzz_config.sh
+++ docker/fuzz_config.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 AFL_SKIP_CPUFREQ=1 py-afl-fuzz -x /faucet-src/tests/fuzzer/dict/yaml.dict -m 5000 -i /faucet-src/tests/fuzzer/config/ -o /var/log/afl/ -- /usr/bin/python3 /faucet-src/tests/fuzzer/fuzz_config.py
+AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 AFL_SKIP_CPUFREQ=1 py-afl-fuzz -x /faucet-src/tests/fuzzer/dict/config.dict -m 5000 -i /faucet-src/tests/fuzzer/config/ -o /var/log/afl/ -- /usr/bin/python3 /faucet-src/tests/fuzzer/fuzz_config.py
