c118d7605243bc5bb6cedf189d44fc2722330931
cglewis <clewis@iqt.org>
2017-12-13 20:52:47 -0500

    adjust entrypoint permissions; add group

--- docker/base/entrypoint.sh
+++ docker/base/entrypoint.sh
@@ -7,14 +7,9 @@
 USER_ID=${LOCAL_USER_ID:-0}
 GROUP_ID=${LOCAL_GROUP_ID:-0}
 
-echo "Starting with UID : $USER_ID GID=$GROUP_ID"
+echo "Starting with UID=$USER_ID GID=$GROUP_ID"
 export HOME=/home/faucet
+addgroup -g $GROUP_ID faucet >/dev/null 2>&1 || true
 adduser -u $USER_ID -G faucet -g "" -h $HOME -D faucet >/dev/null 2>&1 || true
-
-# Update permissions on directories installed by pip before we su-exec
-mkdir -p /var/run/faucet
-chown faucet:faucet /var/run/faucet/
-chown faucet:faucet /var/log/ryu/faucet/
-chown -R faucet:faucet /etc/ryu/faucet/
 
 exec /sbin/su-exec faucet "$@"
