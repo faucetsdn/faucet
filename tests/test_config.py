a4fd599d38f4d9640ad1b3b1fbaca8545627dbfb
mab68 <mab68@students.waikato.ac.nz>
2017-12-14 14:01:54 +1300

    Fix #1399

--- tests/test_config.py
+++ tests/test_config.py
@@ -771,6 +771,21 @@ dps:
 """
         self.check_config_failure(config, cp.dp_parser)
 
+    def test_dp_id_not_a_string(self):
+        """Test dp_id is not a string"""
+        config = """
+vlans:
+    office:
+        vid: 100
+dps:
+    sw1:
+        dp_id: &x1
+        interfaces:
+            1:
+                native_vlan: office        
+"""
+        self.check_config_failure(config, cp.dp_parser)
+
 
 if __name__ == "__main__":
     unittest.main()
