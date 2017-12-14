9cebc5cad29b990a68a1abb0762d73a144a693b2
mab68 <mab68@students.waikato.ac.nz>
2017-12-14 15:02:43 +1300

    Fix #1404

--- tests/test_config.py
+++ tests/test_config.py
@@ -862,6 +862,22 @@ dps:
 """
         self.check_config_failure(config, cp.dp_parser)
 
+    def test_invalid_include_values(self):
+        """Test include directive contains invalid values"""
+        config = """
+include:
+vlans:
+    office:
+        vid: 100
+dps:
+    sw1:
+        dp_id: 0x1
+        interfaces:
+            1:
+                native_vlan: office
+"""
+        self.check_config_failure(config, cp.dp_parser)
+
 
 if __name__ == "__main__":
     unittest.main()
