a9dc0e779a6c22330a9ca9fb649136585987cfa8
Josh Bailey <joshb@google.com>
2018-01-04 13:20:45 +1300

    Use common FROOT.

--- docker/base/install-faucet.sh
+++ docker/base/install-faucet.sh
@@ -12,7 +12,7 @@ $APK add -U git $BUILDDEPS && \
   $dir/retrycmd.sh "$PIP3 pip" && \
   $dir/retrycmd.sh "$PIP3 $TESTDEPS" && \
   $dir/retrycmd.sh "$PIP3 -r $FROOT/requirements.txt" && \
-  $PIP3 /faucet-src && \
+  $PIP3 $FROOT && \
   python3 -m pytest $FROOT/tests/test_valve.py && \
   for i in $BUILDDEPS ; do $APK del $i ; done && \
   find / -name \*pyc -delete
