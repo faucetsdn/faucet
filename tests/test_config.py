320dc0a2ef9a89d66a2b6db9275e3c811cefaf8a
Josh Bailey <joshb@google.com>
2018-01-13 11:27:22 +1300

    Test cannot have same VLAN tagged and native on same port.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1299,6 +1299,22 @@ dps:
 """
         self.check_config_failure(config, cp.dp_parser)
 
+    def test_same_vlan_tagged_untagged(self):
+        """Test cannot have the same VLAN tagged and untagged on same port."""
+        config = """
+vlans:
+    guest:
+        vid: 100
+dps:
+    sw1:
+        dp_id: 0x1
+        interfaces:
+            1:
+                native_vlan: 100
+                tagged_vlans: [100]
+"""
+        self.check_config_failure(config, cp.dp_parser)
+
 
 if __name__ == "__main__":
     unittest.main()
