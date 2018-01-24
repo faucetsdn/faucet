40dc2b756dd4864c42111df729ed147a4f1f5828
Brendon Jones <brendon.jones@gmail.com>
2018-01-25 12:20:13 +1300

    Tests for LLDP defaults.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1472,6 +1472,90 @@ dps:
 """
         self.check_config_success(config, cp.dp_parser)
 
+    def test_dp_lldp_minimal_invalid(self):
+        """Test minimal invalid DP config."""
+        config = """
+vlans:
+    office:
+        vid: 100
+dps:
+    sw1:
+        dp_id: 0x1
+        lldp_beacon:
+            system_name: test_system
+        interfaces:
+            testing:
+                number: 1
+                native_vlan: office
+"""
+        self.check_config_failure(config, cp.dp_parser)
+
+    def test_dp_lldp_minimal_valid(self):
+        """Test minimal valid DP config."""
+        config = """
+vlans:
+    office:
+        vid: 100
+dps:
+    sw1:
+        dp_id: 0x1
+        lldp_beacon:
+            send_interval: 10
+            max_per_interval: 10
+        interfaces:
+            testing:
+                number: 1
+                native_vlan: office
+"""
+        self.check_config_success(config, cp.dp_parser)
+
+    def test_port_lldp_minimal_valid(self):
+        """Test minimal valid LLDP config."""
+        config = """
+vlans:
+    office:
+        vid: 100
+dps:
+    sw1:
+        dp_id: 0x1
+        lldp_beacon:
+            send_interval: 10
+            max_per_interval: 10
+        interfaces:
+            testing:
+                number: 1
+                native_vlan: office
+                lldp_beacon:
+                    enable: true
+"""
+        self.check_config_success(config, cp.dp_parser)
+
+    def test_all_lldp_valid(self):
+        """Test a fully specified valid LLDP config."""
+        config = """
+vlans:
+    office:
+        vid: 100
+dps:
+    sw1:
+        dp_id: 0x1
+        lldp_beacon:
+            system_name: test_system
+            send_interval: 10
+            max_per_interval: 10
+        interfaces:
+            testing:
+                number: 1
+                native_vlan: office
+                lldp_beacon:
+                    enable: true
+                    system_name: port_system
+                    port_descr: port_description
+                    org_tlvs:
+                        - {oui: 0x12bb, subtype: 2, info: "01406500"}
+"""
+        self.check_config_success(config, cp.dp_parser)
+
 
 if __name__ == "__main__":
     unittest.main()
