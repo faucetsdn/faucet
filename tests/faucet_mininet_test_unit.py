62cc903c1b3f4ea921bccbc61e60d8872eba8301
Josh Bailey <joshb@google.com>
2018-03-02 14:51:48 +1300

    Reduce L2 scale from 4096 to 3072.

--- tests/faucet_mininet_test_unit.py
+++ tests/faucet_mininet_test_unit.py
@@ -1363,7 +1363,7 @@ vlans:
         test_net = ipaddress.IPv4Network(
             u'%s/%s' % (self.TEST_IPV4_NET, self.TEST_IPV4_PREFIX))
         learn_ip = ipaddress.IPv4Address(self.LEARN_IPV4)
-        self.verify_learning(test_net, learn_ip, 64, 4096)
+        self.verify_learning(test_net, learn_ip, 64, 3072)
 
 
 class FaucetUntaggedHUPTest(FaucetUntaggedTest):
