6b54f0d362c0d463d8e6ae58be2a13b4ce34c6da
Josh Bailey <joshb@google.com>
2018-03-02 15:32:07 +1300

    Increase L2 scale to 4096.

--- tests/faucet_mininet_test_unit.py
+++ tests/faucet_mininet_test_unit.py
@@ -1364,7 +1364,7 @@ vlans:
         test_net = ipaddress.IPv4Network(
             u'%s/%s' % (self.TEST_IPV4_NET, self.TEST_IPV4_PREFIX))
         learn_ip = ipaddress.IPv4Address(self.LEARN_IPV4)
-        self.verify_learning(test_net, learn_ip, 64, 3072)
+        self.verify_learning(test_net, learn_ip, 64, 4096)
 
 
 class FaucetUntaggedHUPTest(FaucetUntaggedTest):
