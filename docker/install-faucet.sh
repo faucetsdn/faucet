ac4781f5fb72260c3d1951a1a067fd93ca90b34f
Brad Cowie <brad@wand.net.nz>
2018-05-25 17:08:09 +1200

    Skip tests on armhf docker builds.

--- docker/install-faucet.sh
+++ docker/install-faucet.sh
@@ -15,7 +15,16 @@ $APK add -U git yaml-dev $BUILDDEPS && \
   $dir/retrycmd.sh "$PIP3 pip" && \
   $dir/retrycmd.sh "$PIP3 $TESTDEPS" && \
   $dir/retrycmd.sh "$PIP3 -r $FROOT/requirements.txt" && \
-  $PIP3 $FROOT && \
-  python3 -m pytest $FROOT/tests/test_valve.py && \
-  for i in $BUILDDEPS ; do $APK del $i ; done && \
-  find / -name \*pyc -delete
+  $PIP3 $FROOT
+
+if [ "$ARCH" == "armhf" ]; then
+  echo "Skipping tests on $ARCH platform"
+else
+  python3 -m pytest $FROOT/tests/test_valve.py
+fi
+
+for i in $BUILDDEPS ; do
+  $APK del $i
+done
+
+find / -name \*pyc -delete
