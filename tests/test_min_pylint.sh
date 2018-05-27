7708f60520ae54f18db353c60012d9b0eb66f3d6
Josh Bailey <joshb@google.com>
2018-05-28 11:50:47 +1200

    Run pylint on clib as well.

--- tests/test_min_pylint.sh
+++ tests/test_min_pylint.sh
@@ -2,13 +2,13 @@
 
 MINRATING="9.2"
 FAUCETHOME=`dirname $0`"/.."
+PYTHONPATH=$FAUCETHOME:$FAUCETHOME/clib
 
-# TODO: add clib
-for src_dir in faucet tests ; do
+for src_dir in clib faucet tests ; do
     src_files=$FAUCETHOME/$src_dir/[a-z]*.py
 
     for f in $src_files ; do
-        rating=`PYTHONPATH=$FAUCETHOME pylint --rcfile=/dev/null $f | grep -ohE "rated at [0-9\.]+" | sed "s/rated at //g"`
+        rating=`PYTHONPATH=$PYTHONPATH pylint --rcfile=/dev/null $f | grep -ohE "rated at [0-9\.]+" | sed "s/rated at //g"`
         echo pylint $f: $rating
         if [ $(bc <<< "$rating < $MINRATING") -eq 1 ] ; then
             echo "$rating below min ($MINRATING)"
