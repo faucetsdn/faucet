e8b6b8197d7f9ae250a3f768409842382fd99673
Josh Bailey <joshb@google.com>
2018-05-29 12:19:00 +1200

    Of course, config should be updated with a write/rename, not in place.

--- clib/mininet_test_base.py
+++ clib/mininet_test_base.py
@@ -1038,14 +1038,13 @@ dbs:
 
         def _update_conf(conf_path, yaml_conf):
             if yaml_conf:
-                orig_mtime = os.path.getmtime(conf_path)
-                with open(conf_path, 'w', buffering=0) as config_file:
+                conf_path_tmp = tempfile.NamedTemporaryFile(
+                    prefix=os.path.basename(conf_path),
+                    dir=os.path.dirname(conf_path),
+                    delete=False)
+                with open(conf_path_tmp, 'w', buffering=0) as config_file:
                     config_file.write(yaml.dump(yaml_conf))
-                for _ in range(3):
-                    if orig_mtime < os.path.getmtime(conf_path):
-                        return
-                    time.sleep(1)
-                self.fail('could not write %s' % conf_path)
+                os.rename(conf_path_tmp, conf_path)
 
         update_conf_func = partial(_update_conf, conf_path, yaml_conf)
         verify_faucet_reconf_func = partial(
