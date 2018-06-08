7214202ef3a570f4f8a27821f7eb39f0977dbd35
Josh Bailey <joshb@google.com>
2018-06-08 15:39:22 +1200

    Wrong "-d" flag passed to docker tests.

--- run-travis-test.sh
+++ run-travis-test.sh
@@ -5,14 +5,13 @@ docker build -t ${FAUCET_TEST_IMG} -f Dockerfile.tests . || exit 1
 docker rmi faucet/test-base
 docker images
 
-ALLTESTS=""
-for i in tests/faucet_mininet_test_unit.py clib/clib_mininet_test_unit.py ; do
-  ALLTESTS=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" $i|cut -f2 -d" "|sort`" "
-done
-
 if [ "${MATRIX_SHARD}" == "sanity" ] ; then
   RUNTESTS="FaucetSanityTest"
 else:
+  ALLTESTS=""
+  for i in tests/faucet_mininet_test_unit.py clib/clib_mininet_test_unit.py ; do
+    ALLTESTS+=`grep -E -o "^class (Faucet[a-zA-Z0-9]+Test)" $i|cut -f2 -d" "|sort`" "
+  done
   declare -A sharded
 
   function shard {
@@ -30,4 +29,4 @@ else:
   RUNTESTS="-i ${sharded[${MATRIX_SHARD}]}"
 fi
 
-sudo docker run --privileged --sysctl net.ipv6.conf.all.disable_ipv6=0 -t -e FAUCET_TESTS="-d ${RUNTESTS}" ${FAUCET_TEST_IMG}
+sudo docker run --privileged --sysctl net.ipv6.conf.all.disable_ipv6=0 -t -e FAUCET_TESTS="${RUNTESTS}" ${FAUCET_TEST_IMG}
