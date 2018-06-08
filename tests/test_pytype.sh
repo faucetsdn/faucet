6fdbed98418fd48c6e91a71311500c98a4f95434
Josh Bailey <joshb@google.com>
2018-06-08 17:58:40 +1200

    Fix delim for parallel.

--- tests/test_pytype.sh
+++ tests/test_pytype.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 FAUCETHOME=`dirname $0`"/.."
-echo `$FAUCETHOME/tests/src_files.sh` | parallel --delim " " --delay 1 --bar pytype -d pyi-error,import-error || exit 1
+$FAUCETHOME/tests/src_files.sh | parallel --delay 1 --bar pytype -d pyi-error,import-error || exit 1
 exit 0
