13a3aaca90cc596b16d609307427b7a636a8c7b4
Josh Bailey <joshb@google.com>
2018-06-08 16:32:37 +1200

    avoid egg cache dir collision.

--- tests/test_pytype.sh
+++ tests/test_pytype.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 FAUCETHOME=`dirname $0`"/.."
-echo `$FAUCETHOME/tests/src_files.sh` | parallel --delim " " --bar pytype -d pyi-error,import-error || exit 1
+echo `$FAUCETHOME/tests/src_files.sh` | parallel --delim " " --delay 1 --bar pytype -d pyi-error,import-error || exit 1
 exit 0
