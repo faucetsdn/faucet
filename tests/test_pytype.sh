cb7b3302a8f9446716fe7552acad3ebe1b04e672
Josh Bailey <joshb@google.com>
2018-06-11 22:31:29 +1200

    Automatically do pytype for python2 if mininet is imported.

--- tests/test_pytype.sh
+++ tests/test_pytype.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 FAUCETHOME=`dirname $0`"/.."
-# TODO: remove workaround for mininet_test.py
-$FAUCETHOME/tests/src_files.sh | grep -v mininet_test.py | parallel --delay 1 -j 2 --bar pytype -V3.5 -d pyi-error,import-error || exit 1
-exit 0
+PARGS='--delay 1 -j 2 --bar pytype -d pyi-error,import-error'
+
+PY2=""
+PY3=""
+for i in `$FAUCETHOME/tests/src_files.sh` ; do
+  if grep -qn "import mininet" $i ; then
+    PY2+="$i\n"
+  else
+    PY3+="$i\n"
+  fi
+done
+
+echo -e $PY2 | parallel $PARGS -V2.7 || exit 1
+echo -e $PY3 | parallel $PARGS -V3.5 || exit 1
