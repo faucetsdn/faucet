52e21b7a1a3de9f62195c48e29a381f8d0f01517
Josh Bailey <joshb@google.com>
2018-06-11 22:04:17 +1200

    workaround for mininet_test.py.

--- tests/test_pytype.sh
+++ tests/test_pytype.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 FAUCETHOME=`dirname $0`"/.."
-$FAUCETHOME/tests/src_files.sh | parallel --delay 1 -j 2 --bar pytype -V3.5 -d pyi-error,import-error || exit 1
+# TODO: remove workaround for mininet_test.py
+$FAUCETHOME/tests/src_files.sh | grep -v mininet_test.py | parallel --delay 1 -j 2 --bar pytype -V3.5 -d pyi-error,import-error || exit 1
 exit 0
