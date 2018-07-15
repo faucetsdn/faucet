264b116146740c1bc6322ae82ec421e0d161834d
Josh Bailey <joshb@google.com>
2018-07-15 22:24:08 +1200

    Fix pip/pip3.

--- docker/pip_deps.sh
+++ docker/pip_deps.sh
@@ -3,8 +3,8 @@
 FAUCETHOME=`dirname $0`/..
 PIPARGS=$*
 
-$FAUCETHOME/docker/retrycmd.sh "$pip install --upgrade $PIPARGS -r $FAUCETHOME/py2-test-requirements.txt" || exit 1
+$FAUCETHOME/docker/retrycmd.sh "pip install --upgrade $PIPARGS -r $FAUCETHOME/py2-test-requirements.txt" || exit 1
 
 for r in test-requirements.txt requirements.txt docs/requirements.txt fuzz-requirements.txt ; do
-  $FAUCETHOME/docker/retrycmd.sh "$pip3 install --upgrade $PIPARGS -r $FAUCETHOME/$r" || exit 1
+  $FAUCETHOME/docker/retrycmd.sh "pip3 install --upgrade $PIPARGS -r $FAUCETHOME/$r" || exit 1
 done
