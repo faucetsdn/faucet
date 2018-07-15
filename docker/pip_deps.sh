1f5fc609cf8b2186e8158ae43a31be64acd6aed8
Josh Bailey <joshb@google.com>
2018-07-15 21:06:05 +1200

    Disable pip quiet.

--- docker/pip_deps.sh
+++ docker/pip_deps.sh
@@ -5,6 +5,6 @@ PIPARGS=$*
 
 for p in pip pip3 ; do
   for r in test-requirements.txt requirements.txt docs/requirements.txt fuzz-requirements.txt ; do
-    $FAUCETHOME/docker/retrycmd.sh "$p install -q --upgrade $PIPARGS -r $FAUCETHOME/$r" || exit 1
+    $FAUCETHOME/docker/retrycmd.sh "$p install --upgrade $PIPARGS -r $FAUCETHOME/$r" || exit 1
   done
 done
