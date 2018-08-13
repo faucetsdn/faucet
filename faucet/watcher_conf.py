23d0a420bad9a19f00234f1e53b401eb4dc2b03d
Josh Bailey <joshb@google.com>
2018-08-14 11:42:49 +1200

    attributes.

--- faucet/watcher_conf.py
+++ faucet/watcher_conf.py
@@ -76,15 +76,6 @@ For Prometheus:
        Defaults to '127.0.0.1'.
 """
 
-    db = None # pylint: disable=invalid-name
-    dp = None # pylint: disable=invalid-name
-    all_dps = None
-    prom_client = None
-    type = None
-    interval = None
-    db_type = None
-    dps = None
-
     defaults = {
         'name': None,
         'type': None,
@@ -139,9 +130,30 @@ For Prometheus:
     }
 
     def __init__(self, _id, dp_id, conf, prom_client):
+        self.db = None # pylint: disable=invalid-name
+        self.dbs = None
+        self.dp = None # pylint: disable=invalid-name
+        self.all_dps = None
+        self.type = None
+        self.interval = None
+        self.db_type = None
+        self.dps = None
+        self.compress = None
+        self.file = None
+        self.influx_db = None
+        self.influx_host = None
+        self.influx_port = None
+        self.influx_user = None
+        self.influx_pwd = None
+        self.influx_timeout = None
+        self.influx_retries = None
+        self.name = None
+        self.prometheus_port = None
+        self.prometheus_addr = None
+        self.prometheus_test_thread = None
         super(WatcherConf, self).__init__(_id, dp_id, conf)
-        self.prom_client = prom_client
         self.name = str(self._id)
+        self.prom_client = prom_client
 
     def add_db(self, db_conf):
         """Add database config to this watcher."""
