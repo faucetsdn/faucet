2e8e67e54cb1959711cfb86f0556a96878bdeee9
Josh Bailey <joshb@google.com>
2018-08-27 08:27:04 +1200

    Don't skip test_packaging.py (works fine when -V3.5 is specified).

--- tests/codecheck/src_files.sh
+++ tests/codecheck/src_files.sh
@@ -2,4 +2,4 @@
 
 FAUCETHOME=`dirname $0`"/../.."
 # TODO: test_packaging.py causes pytype to die.
-for i in clib faucet tests ; do find $FAUCETHOME/$i/ -type f -name [a-z]*.py ; done | xargs realpath | sort |grep -v test_packaging.py
+for i in clib faucet tests ; do find $FAUCETHOME/$i/ -type f -name [a-z]*.py ; done | xargs realpath | sort
