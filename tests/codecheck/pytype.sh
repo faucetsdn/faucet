9632d5a558b6727dff50d82a6ada67bad850aaad
Josh Bailey <joshb@google.com>
2018-09-10 09:28:01 +1200

    Revert "Still need to use parallel for pytype as pytype uses lots of memory."
    This reverts commit 13639dc04ec64895073bec0cfc8b9370169e3abe.

--- tests/codecheck/pytype.sh
+++ tests/codecheck/pytype.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# TODO: pytype is very memory intensive - run per separate directory to limit resources.
 # TODO: until 3.6 safe, force 3.5
 PYV="3.5"
 FAUCETHOME=`dirname $0`"/../.."
@@ -14,21 +13,15 @@ function cleanup {
 }
 trap cleanup EXIT
 
-PYTYPEARGS="pytype -V$PYV -o $TMPD --pythonpath $PYTHONPATH -d pyi-error,import-error"
+PYTYPEARGS="pytype -V$PYV -o $TMPD --pythonpath $PYTHONPATH -d pyi-error,import-error -v2"
 
 declare -A SRCDIRS
 for f in `$SRCFILES` ; do
    d=`dirname $f`
-   if [ "SRCDIRS[$d]]" ] ; then
-       SRCDIRS[$d]="$f\n${SRCDIRS[$d]}"
-   else
-       SRCDIRS[$d]=$f
-   fi
+   SRCDIRS[$d]="$f ${SRCDIRS[$d]}"
 done
 
-PARARGS="parallel --delay 1 --bar"
 for d in "${!SRCDIRS[@]}" ; do
    srcfiles=${SRCDIRS[$d]}
-   echo -e $d: $srcfiles
-   echo -e $srcfiles | $PARARGS $PYTYPEARGS || exit 1
+   $PYTYPEARGS $srcfiles || exit 1
 done
