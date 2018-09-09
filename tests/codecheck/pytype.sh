13639dc04ec64895073bec0cfc8b9370169e3abe
Josh Bailey <joshb@google.com>
2018-09-10 09:20:00 +1200

    Still need to use parallel for pytype as pytype uses lots of memory.

--- tests/codecheck/pytype.sh
+++ tests/codecheck/pytype.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO: pytype is very memory intensive - run per separate directory to limit resources.
 # TODO: until 3.6 safe, force 3.5
 PYV="3.5"
 FAUCETHOME=`dirname $0`"/../.."
@@ -13,15 +14,21 @@ function cleanup {
 }
 trap cleanup EXIT
 
-PYTYPEARGS="pytype -V$PYV -o $TMPD --pythonpath $PYTHONPATH -d pyi-error,import-error -v2"
+PYTYPEARGS="pytype -V$PYV -o $TMPD --pythonpath $PYTHONPATH -d pyi-error,import-error"
 
 declare -A SRCDIRS
 for f in `$SRCFILES` ; do
    d=`dirname $f`
-   SRCDIRS[$d]="$f ${SRCDIRS[$d]}"
+   if [ "SRCDIRS[$d]]" ] ; then
+       SRCDIRS[$d]="$f\n${SRCDIRS[$d]}"
+   else
+       SRCDIRS[$d]=$f
+   fi
 done
 
+PARARGS="parallel --delay 1 --bar"
 for d in "${!SRCDIRS[@]}" ; do
    srcfiles=${SRCDIRS[$d]}
-   $PYTYPEARGS $srcfiles || exit 1
+   echo -e $d: $srcfiles
+   echo -e $srcfiles | $PARARGS $PYTYPEARGS || exit 1
 done
