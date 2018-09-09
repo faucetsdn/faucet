16dcefd592e87b92750f8ca2d1c4f793c0327286
Josh Bailey <joshb@google.com>
2018-09-10 09:04:10 +1200

    Run pytype by top level directory to limit memory usage.

--- tests/codecheck/pytype.sh
+++ tests/codecheck/pytype.sh
@@ -4,7 +4,6 @@
 PYV="3.5"
 FAUCETHOME=`dirname $0`"/../.."
 PYTHONPATH=$FAUCETHOME:$FAUCETHOME/faucet:$FAUCETHOME/clib
-PARARGS="parallel --delay 1 --bar"
 SRCFILES="$FAUCETHOME/tests/codecheck/src_files.sh"
 
 TMPD=`mktemp -d`
@@ -14,5 +13,15 @@ function cleanup {
 }
 trap cleanup EXIT
 
-PYTYPEARGS="pytype -V$PYV -o $TMPD --pythonpath $PYTHONPATH -d pyi-error,import-error"
-$PYTYPEARGS `$SRCFILES` || exit 1
+PYTYPEARGS="pytype -V$PYV -o $TMPD --pythonpath $PYTHONPATH -d pyi-error,import-error -v2"
+
+declare -A SRCDIRS
+for f in `$SRCFILES` ; do
+   d=`dirname $f`
+   SRCDIRS[$d]="$f ${SRCDIRS[$d]}"
+done
+
+for d in "${!SRCDIRS[@]}" ; do
+   srcfiles=${SRCDIRS[$d]}
+   $PYTYPEARGS $srcfiles || exit 1
+done
