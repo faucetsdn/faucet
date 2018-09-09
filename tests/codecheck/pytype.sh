27f93731f37e9c993afd53cccc2867b06f7bf69f
Josh Bailey <joshb@google.com>
2018-09-10 08:49:52 +1200

    https://github.com/google/pytype/issues/133 is fixed so don't have to run under parallel.

--- tests/codecheck/pytype.sh
+++ tests/codecheck/pytype.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# TODO: run separately until https://github.com/google/pytype/issues/133 is fixed.
 # TODO: until 3.6 safe, force 3.5
 PYV="3.5"
 FAUCETHOME=`dirname $0`"/../.."
 PYTHONPATH=$FAUCETHOME:$FAUCETHOME/faucet:$FAUCETHOME/clib
 PARARGS="parallel --delay 1 --bar"
-PYTYPE=`which pytype`
-PYTYPEARGS="python$PYV  $PYTYPE --pythonpath $PYTHONPATH -d pyi-error,import-error -V$PYV"
-PYHEADER=`head -1 $PYTYPE`
 SRCFILES="$FAUCETHOME/tests/codecheck/src_files.sh"
-echo "Using $PYTYPE (header $PYHEADER)"
 
-$SRCFILES | shuf | $PARARGS $PYTYPEARGS || exit 1
+TMPD=`mktemp -d`
+test -d $TMPD || exit
+function cleanup {
+  rm -r "$TMPD"
+}
+trap cleanup EXIT
+
+PYTYPEARGS="pytype -V$PYV -o $TMPD --pythonpath $PYTHONPATH -d pyi-error,import-error"
+$PYTYPEARGS `$SRCFILES` || exit 1
