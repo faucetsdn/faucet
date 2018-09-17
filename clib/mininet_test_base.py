35bf12966f3852e5d22fee16061b272c51af0ae5
Josh Bailey <joshb@google.com>
2018-09-17 17:58:05 +1200

    More iperf diagnostics.

--- clib/mininet_test_base.py
+++ clib/mininet_test_base.py
@@ -2189,6 +2189,7 @@ dbs:
         iperf_csv = iperf_results.strip().split(',')
         if len(iperf_csv) == 9:
             return int(iperf_csv[-1]) / self.ONEMBPS
+        error('%s: %s %s' % (self._test_name(), iperf_client_cmd, iperf_results))
         return -1
 
     def iperf(self, client_host, client_ip, server_host, server_ip, seconds):
@@ -2203,14 +2204,15 @@ dbs:
             for host, line in pmonitor(popens):
                 if host != server_host:
                     continue
-                error('%s: %s' % (self._test_name(), line))
-                if re.search(server_start_exp, line):
-                    self.wait_for_tcp_listen(
-                        server_host, port, ipv=server_ip.version)
-                    iperf_mbps = self.iperf_client(
-                        client_host, iperf_client_cmd)
-                    self._signal_proc_on_port(server_host, port, 9)
-                    return iperf_mbps
+                error('%s: %s %s' % (self._test_name(), iperf_server_cmd, line))
+                if not re.search(server_start_exp, line):
+                    continue
+                self.wait_for_tcp_listen(
+                    server_host, port, ipv=server_ip.version)
+                iperf_mbps = self.iperf_client(
+                    client_host, iperf_client_cmd)
+                self._signal_proc_on_port(server_host, port, 9)
+                return iperf_mbps
             return None
 
         timeout = (seconds * 3) + 5
