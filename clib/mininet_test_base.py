b960e2b0882a7aa6e7696c6f462529b5bc63d725
Josh Bailey <joshb@google.com>
2018-09-26 11:46:17 +1200

    flow dumper now requires table ID always, and keeps most recent table dump.

--- clib/mininet_test_base.py
+++ clib/mininet_test_base.py
@@ -687,14 +687,13 @@ dbs:
         return self._ofctl_get(
             int_dpid, 'stats/groupdesc/%s' % int_dpid, timeout)
 
-    def get_all_flows_from_dpid(self, dpid, timeout=10, table_id=None, match=None):
+    def get_all_flows_from_dpid(self, dpid, table_id, timeout=10, match=None):
         """Return all flows from DPID."""
         int_dpid = mininet_test_util.str_int_dpid(dpid)
         params = {}
-        if table_id is not None:
-            params[u'table_id'] = table_id
+        params['table_id'] = table_id
         if match is not None:
-            params[u'match'] = match
+            params['match'] = match
         return self._ofctl_post(
             int_dpid, 'stats/flow/%s' % int_dpid, timeout, params=params)
 
@@ -734,8 +733,9 @@ dbs:
             time.sleep(1)
         return False
 
-    def get_matching_flows_on_dpid(self, dpid, match, timeout=10, table_id=None,
-                                   actions=None, hard_timeout=0, cookie=None, ofa_match=True):
+    def get_matching_flows_on_dpid(self, dpid, match, table_id, timeout=10, 
+                                   actions=None, hard_timeout=0, cookie=None,
+                                   ofa_match=True):
 
         # TODO: Ryu ofctl serializes to old matches.
         def to_old_match(match):
@@ -752,7 +752,7 @@ dbs:
                         del match[new_match]
             return match
 
-        flowdump = os.path.join(self.tmpdir, 'flowdump-%s.txt' % dpid)
+        flowdump = os.path.join(self.tmpdir, 'flowdump-%s.log' % dpid)
         match = to_old_match(match)
         match_set = None
         if match:
@@ -761,65 +761,64 @@ dbs:
         if actions:
             actions_set = frozenset(actions)
 
-        with open(flowdump, 'w') as flowdump_file:
-            for _ in range(timeout):
-                flow_dicts = []
-                if ofa_match:
-                    flow_dump = self.get_all_flows_from_dpid(dpid, table_id=table_id, match=match)
-                else:
-                    flow_dump = self.get_all_flows_from_dpid(dpid, table_id=table_id)
+        for _ in range(timeout):
+            flow_dicts = []
+            if ofa_match:
+                flow_dump = self.get_all_flows_from_dpid(dpid, table_id, match=match)
+            else:
+                flow_dump = self.get_all_flows_from_dpid(dpid, table_id)
+            with open(flowdump, 'w') as flowdump_file:
                 flowdump_file.write('\n'.join(str(flow_dump)))
-                for flow_dict in flow_dump:
-                    if (cookie is not None and
-                            cookie != flow_dict['cookie']):
+            for flow_dict in flow_dump:
+                if (cookie is not None and
+                        cookie != flow_dict['cookie']):
+                    continue
+                if hard_timeout:
+                    if not 'hard_timeout' in flow_dict:
                         continue
-                    if hard_timeout:
-                        if not 'hard_timeout' in flow_dict:
+                    if flow_dict['hard_timeout'] < hard_timeout:
+                        continue
+                if actions is not None:
+                    flow_actions_set = frozenset(flow_dict['actions'])
+                    if actions:
+                        if not actions_set.issubset( # pytype: disable=attribute-error
+                                flow_actions_set):
                             continue
-                        if flow_dict['hard_timeout'] < hard_timeout:
+                    else:
+                        if flow_dict['actions']:
                             continue
-                    if actions is not None:
-                        flow_actions_set = frozenset(flow_dict['actions'])
-                        if actions:
-                            if not actions_set.issubset( # pytype: disable=attribute-error
-                                    flow_actions_set):
-                                continue
-                        else:
-                            if flow_dict['actions']:
-                                continue
-                    if not ofa_match and match is not None:
-                        flow_match_set = frozenset(flow_dict['match'].items())
-                        if not match_set.issubset(flow_match_set): # pytype: disable=attribute-error
-                            continue
-                    flow_dicts.append(flow_dict)
-                if flow_dicts:
-                    return flow_dicts
-                time.sleep(1)
+                if not ofa_match and match is not None:
+                    flow_match_set = frozenset(flow_dict['match'].items())
+                    if not match_set.issubset(flow_match_set): # pytype: disable=attribute-error
+                        continue
+                flow_dicts.append(flow_dict)
+            if flow_dicts:
+                return flow_dicts
+            time.sleep(1)
         return flow_dicts
 
-    def get_matching_flow_on_dpid(self, dpid, match, timeout=10, table_id=None,
+    def get_matching_flow_on_dpid(self, dpid, match, table_id, timeout=10,
                                   actions=None, hard_timeout=0, cookie=None,
                                   ofa_match=True):
         flow_dicts = self.get_matching_flows_on_dpid(
-            dpid, match, timeout=timeout, table_id=table_id,
+            dpid, match, table_id, timeout=timeout,
             actions=actions, hard_timeout=hard_timeout, cookie=cookie,
             ofa_match=ofa_match)
         if flow_dicts:
             return flow_dicts[0]
         return []
 
-    def get_matching_flow(self, match, timeout=10, table_id=None,
+    def get_matching_flow(self, match, table_id, timeout=10,
                           actions=None, hard_timeout=0,
                           cookie=None, ofa_match=True):
         return self.get_matching_flow_on_dpid(
-            self.dpid, match, timeout=timeout, table_id=table_id,
+            self.dpid, match, table_id, timeout=timeout,
             actions=actions, hard_timeout=hard_timeout,
             cookie=cookie, ofa_match=ofa_match)
 
-    def get_group_id_for_matching_flow(self, match, timeout=10, table_id=None):
+    def get_group_id_for_matching_flow(self, match, table_id, timeout=10):
         for _ in range(timeout):
-            flow_dict = self.get_matching_flow(
-                match, timeout=timeout, table_id=table_id)
+            flow_dict = self.get_matching_flow(match, table_id, timeout=timeout)
             if flow_dict:
                 for action in flow_dict['actions']:
                     if action.startswith('GROUP'):
@@ -828,33 +827,33 @@ dbs:
             time.sleep(1)
         return None
 
-    def matching_flow_present_on_dpid(self, dpid, match, timeout=10, table_id=None,
+    def matching_flow_present_on_dpid(self, dpid, match, table_id, timeout=10,
                                       actions=None, hard_timeout=0, cookie=None,
                                       ofa_match=True):
         """Return True if matching flow is present on a DPID."""
         if self.get_matching_flow_on_dpid(
-                dpid, match, timeout=timeout, table_id=table_id,
+                dpid, match, table_id, timeout=timeout,
                 actions=actions, hard_timeout=hard_timeout, cookie=cookie,
                 ofa_match=ofa_match):
             return True
         return False
 
-    def matching_flow_present(self, match, timeout=10, table_id=None,
+    def matching_flow_present(self, match, table_id, timeout=10,
                               actions=None, hard_timeout=0, cookie=None,
                               ofa_match=True):
         """Return True if matching flow is present on default DPID."""
         return self.matching_flow_present_on_dpid(
-            self.dpid, match, timeout=timeout, table_id=table_id,
+            self.dpid, match, table_id, timeout=timeout,
             actions=actions, hard_timeout=hard_timeout, cookie=cookie,
             ofa_match=ofa_match)
 
-    def wait_until_matching_flow(self, match, timeout=10, table_id=None,
+    def wait_until_matching_flow(self, match, table_id, timeout=10,
                                  actions=None, hard_timeout=0, cookie=None,
                                  ofa_match=True):
         """Wait (require) for flow to be present on default DPID."""
         self.assertTrue(
             self.matching_flow_present(
-                match, timeout=timeout, table_id=table_id,
+                match, table_id, timeout=timeout,
                 actions=actions, hard_timeout=hard_timeout, cookie=cookie,
                 ofa_match=ofa_match),
             msg=match)
@@ -874,7 +873,7 @@ dbs:
                     match['in_port'] = in_port
                 match_hard_timeout = hard_timeout
             if not self.matching_flow_present(
-                    match, timeout=timeout, table_id=table_id, hard_timeout=match_hard_timeout):
+                    match, table_id, timeout=timeout, hard_timeout=match_hard_timeout):
                 return False
         return True
 
@@ -1925,14 +1924,14 @@ dbs:
             'echo hello | nc -l %s %u &' % (host.IP(), port), 10))
         self.wait_for_tcp_listen(host, port)
 
-    def wait_nonzero_packet_count_flow(self, match, timeout=15, table_id=None,
+    def wait_nonzero_packet_count_flow(self, match, table_id, timeout=15,
                                        actions=None, dpid=None, ofa_match=True):
         """Wait for a flow to be present and have a non-zero packet_count."""
         if dpid is None:
             dpid = self.dpid
         for _ in range(timeout):
             flow = self.get_matching_flow_on_dpid(
-                dpid, match, timeout=1, table_id=table_id,
+                dpid, match, table_id, timeout=1,
                 actions=actions, ofa_match=ofa_match)
             if flow and flow['packet_count'] > 0:
                 return
@@ -1949,25 +1948,27 @@ dbs:
             first_host,
             (mininet_test_util.timeout_cmd(
                 'nc %s %u' % (second_host.IP(), port), 10), ))
-        if table_id is not None:
-            match = {
-                'dl_type': IPV4_ETH, 'ip_proto': 6
-            }
-            match_port = int(port)
-            if mask is not None:
-                match_port = '/'.join((str(port), str(mask)))
-            match['tp_dst'] = match_port
-            self.wait_nonzero_packet_count_flow(match, table_id=table_id, ofa_match=False)
+        if table_id is None:
+            return
+        match = {
+            'dl_type': IPV4_ETH, 'ip_proto': 6
+        }
+        match_port = int(port)
+        if mask is not None:
+            match_port = '/'.join((str(port), str(mask)))
+        match['tp_dst'] = match_port
+        self.wait_nonzero_packet_count_flow(match, table_id, ofa_match=False)
 
     def verify_tp_dst_notblocked(self, port, first_host, second_host, table_id=0):
         """Verify that a TCP port on a host is NOT blocked from another host."""
         self.serve_hello_on_tcp_port(second_host, port)
         self.assertEqual(
             'hello\r\n',
-            first_host.cmd('nc -w 5 %s %u' % (second_host.IP(), port)))
-        if table_id is not None:
-            self.wait_nonzero_packet_count_flow(
-                {'tp_dst': int(port), 'dl_type': IPV4_ETH, 'ip_proto': 6}, table_id=table_id)
+        first_host.cmd('nc -w 5 %s %u' % (second_host.IP(), port)))
+        if table_id is None:
+            return
+        self.wait_nonzero_packet_count_flow(
+            {'tp_dst': int(port), 'dl_type': IPV4_ETH, 'ip_proto': 6}, table_id)
 
     def bcast_dst_blocked_helper(self, port, first_host, second_host, success_re, retries):
         tcpdump_filter = 'udp and ether src %s and ether dst %s' % (
@@ -2139,11 +2140,11 @@ dbs:
             nw_dst_match['dl_vlan'] = str(vlan_vid)
         if nonzero_packets:
             self.wait_nonzero_packet_count_flow(
-                nw_dst_match, timeout=timeout, table_id=table_id,
+                nw_dst_match, table_id, timeout=timeout,
                 actions=[nexthop_action], ofa_match=False)
         else:
             self.wait_until_matching_flow(
-                nw_dst_match, timeout=timeout, table_id=table_id,
+                nw_dst_match, table_id, timeout=timeout,
                 actions=[nexthop_action], ofa_match=False)
 
     def host_ipv4_alias(self, host, alias_ip, intf=None):
