e887aed77bc16136513051266b0202d02f374ad4
Christopher Lorier <chris.lorier@reannz.co.nz>
2018-09-26 15:48:59 +1200

    remove pointless delete messages

--- faucet/valve.py
+++ faucet/valve.py
@@ -701,8 +701,6 @@ class Valve:
         """Delete flows/state for a port."""
         ofmsgs = []
         ofmsgs.extend(self._delete_all_port_match_flows(port))
-        for table in self.dp.output_tables():
-            ofmsgs.extend(table.flowdel(out_port=port.number))
         if self.dp.egress_pipeline:
             ofmsgs.extend(
                 self.dp.tables['egress'].flowdel(out_port=port.number))
