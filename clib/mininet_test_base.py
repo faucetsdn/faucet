25d2abb1989ee1240ba1e9cee68df1b30c6abb40
Josh Bailey <joshb@google.com>
2018-11-07 17:02:59 +1300

    Need to handle port variables with new labels.

--- clib/mininet_test_base.py
+++ clib/mininet_test_base.py
@@ -1006,6 +1006,17 @@ dbs:
                 prom_line for prom_line in prom_lines if prom_line.startswith(var)]
         return prom_lines
 
+    _PROM_LINE_RE = re.compile(r'^(.+)\s+([0-9\.\-e]+)$')
+
+    def parse_prom_var(self, prom_line):
+        prom_line_match = self._PROM_LINE_RE.match(prom_line)
+        self.assertIsNotNone(
+            prom_line_match,
+            msg='Invalid prometheus line %s' % prom_line)
+        prom_var = prom_line_match.group(1)
+        prom_val = int(float(prom_line_match.group(2)))
+        return (prom_var, prom_val)
+
     def scrape_prometheus_var(self, var, labels=None, any_labels=False, default=None,
                               dpid=True, multiple=False, controller='faucet', retries=3):
         if dpid:
@@ -1027,20 +1038,13 @@ dbs:
                     label_values.append('%s="%s"' % (label, value))
                 label_values_re = r'\{%s\}' % r'\S+'.join(label_values)
         var_re = re.compile(r'^%s%s$' % (var, label_values_re))
-        prom_line_re = re.compile(r'^(.+)\s+([0-9\.\-e]+)$')
         for _ in range(retries):
             results = []
             prom_lines = self.scrape_prometheus(controller, var=var)
             for prom_line in prom_lines:
-                prom_line_match = prom_line_re.match(prom_line)
-                self.assertIsNotNone(
-                    prom_line_match,
-                    msg='Invalid prometheus line %s in %s' % (prom_line, prom_lines))
-                prom_var = prom_line_match.group(1)
-                value = prom_line_match.group(2)
+                prom_var, prom_val = self.parse_prom_var(prom_line)
                 if var_re.match(prom_var):
-                    value_int = int(float(value))
-                    results.append((var, value_int))
+                    results.append((var, prom_val))
                     if not multiple:
                         break
             if results:
@@ -1210,7 +1214,7 @@ dbs:
                 host.cmd(self.scapy_dhcp(mac, host.defaultIntf()))
                 new_locations = set()
                 for line in self.scrape_prometheus(var='learned_macs'):
-                    location, mac_float = line.split(' ')
+                    location, mac_float = self.parse_prom_var(line)
                     if self.mac_from_int(int(float(mac_float))) == mac:
                         new_locations.add(location)
                 if locations != new_locations:
