9cc149e8927843332f013bc127a4928eaaabd316
Brad Cowie <brad@wand.net.nz>
2018-12-11 16:46:46 +1300

    Need sphinx 1.8

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ autodoc_default_flags = ["members", "undoc-members", "show-inheritance"]
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.7'
+needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
