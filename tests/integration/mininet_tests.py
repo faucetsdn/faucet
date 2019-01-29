3a2ee10ae31c1da8ff2e19ef1e8040f92f079fcc
Josh Bailey <joshb@google.com>
2019-01-29 23:05:09 +1300

    Need to retry host retry check.

--- tests/integration/mininet_tests.py
+++ tests/integration/mininet_tests.py
@@ -2892,9 +2892,15 @@ vlans:
         self.assertTrue(re.search('10.0.2.0/24 next-hop 10.0.0.2', updates))
         # test nexthop expired when port goes down
         first_host = self.net.hosts[0]
-        self.one_ipv4_controller_ping(first_host)
         match, table = self.match_table(ipaddress.IPv4Network('10.0.0.1/32'))
-        self.assertTrue(self.get_matching_flow(match, table_id=table))
+        ofmsg = None
+        for _ in range(5):
+            self.one_ipv4_controller_ping(first_host)
+            ofmsg = self.get_matching_flow(match, table_id=table)
+            if ofmsg:
+                break
+            time.sleep(1)
+        self.assertTrue(ofmsg, msg=match)
         self.set_port_down(self.port_map['port_1'])
         for _ in range(5):
             if not self.get_matching_flow(match, table_id=table):
