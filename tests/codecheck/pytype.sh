f1933d47980a48a15d7fa7452e7c5bfced5b9f6e
Brad Cowie <brad@wand.net.nz>
2019-02-26 20:13:45 +1300

    Standardise parallel arguments.

--- tests/codecheck/pytype.sh
+++ tests/codecheck/pytype.sh
@@ -3,8 +3,7 @@
 FAUCETHOME=`dirname $0`"/../.."
 TMPDIR=`mktemp -d -p /var/tmp`
 CONFIG="$FAUCETHOME/setup.cfg"
-# TODO: halt flag must be 2014 parallel compatible.
-PARARGS="parallel --delay 1 --bar --halt 1 -j 2"
+PARARGS="parallel --delay 1 --bar --halt now,fail=1 -j 2"
 PYTYPE=`which pytype`
 PYTYPEARGS="python3 $PYTYPE --config $CONFIG -o $TMPDIR/{/} {}"
 PYHEADER=`head -1 $PYTYPE`
