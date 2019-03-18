d7ab8940785279827b671e95a8419fd9fbbec322
Josh Bailey <joshb@google.com>
2019-03-19 10:46:48 +1300

    set_defaults() can set defaults for sub-dicts.

--- faucet/conf.py
+++ faucet/conf.py
@@ -60,10 +60,20 @@ class Conf:
         else:
             raise ValueError('cannot update %s on finalized Conf object' % name)
 
-    def set_defaults(self):
+    def _set_default(self, key, value, conf=None):
+        if conf is None:
+            conf = self.__dict__
+        assert key in conf, key
+        if conf[key] is None:
+            conf[key] = value
+
+    def _set_conf_defaults(self, defaults, conf):
+        for key, value in defaults.items():
+            self._set_default(key, value, conf=conf)
+
+    def set_defaults(self, defaults=None, conf=None):
         """Set default values and run any basic sanity checks."""
-        for key, value in self.defaults.items():
-            self._set_default(key, value)
+        self._set_conf_defaults(self.defaults, self.__dict__)
 
     def _check_unknown_conf(self, conf):
         """Check that supplied conf dict doesn't specify keys not defined."""
@@ -125,11 +135,6 @@ class Conf:
         """Merge dynamic state from other conf object."""
         self.__dict__.update(
             {k: v for k, v in self._conf_keys(other_conf, dyn=True)})
-
-    def _set_default(self, key, value):
-        assert key in self.__dict__, key
-        if self.__dict__[key] is None:
-            self.__dict__[key] = value
 
     def to_conf(self):
         """Return configuration as a dict."""
