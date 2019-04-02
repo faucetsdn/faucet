7f231b5784c6aa4a2590827637f22fe6587db86f
cglewis <clewis@iqt.org>
2019-04-02 08:38:25 -0700

    add requirements file for rabbitmq for tests

--- docker/pip_deps.sh
+++ docker/pip_deps.sh
@@ -4,6 +4,6 @@ FAUCETHOME=`dirname $0`/..
 FAUCETHOME=`readlink -f $FAUCETHOME`
 PIPARGS="install -q --upgrade $*"
 
-for r in test-requirements.txt fuzz-requirements.txt docs/requirements.txt ; do
+for r in test-requirements.txt fuzz-requirements.txt docs/requirements.txt adapters/vendors/rabbitmq/requirements.txt ; do
   $FAUCETHOME/docker/retrycmd.sh "pip3 $PIPARGS -r $FAUCETHOME/$r" || exit 1
 done
