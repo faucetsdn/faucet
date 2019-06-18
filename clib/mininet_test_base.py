e925b765f3d09336aba867d9c2350ee1c5e675a9
Josh Bailey <joshb@google.com>
2019-06-18 21:51:04 +1200

    Pin exabgp to 4.0.10.
    4.1.0 won't start:
    
    09:43:34 | 658    | configuration   | syntax error in section neighbor/static
    09:43:34 | 658    | configuration   | line 8: route fc00::10:1/112 next-hop fc00
    ::1:1 local-preference 100 ;
    09:43:34 | 658    | configuration   |
    09:43:34 | 658    | configuration   | invalid network fc00::10:1 for netmask 112

--- clib/mininet_test_base.py
+++ clib/mininet_test_base.py
@@ -2188,7 +2188,7 @@ dbs:
         """Start exabgp process on controller host."""
         exabgp_conf_file_name = os.path.join(self.tmpdir, '%sexabgp.conf' % log_prefix)
         exabgp_log = os.path.join(self.tmpdir, '%sexabgp.log' % log_prefix)
-        exabgp_err = os.path.join(self.tmpdir, '%sexabgp.err' % log_prefix)
+        exabgp_out = os.path.join(self.tmpdir, '%sexabgp.out' % log_prefix)
         exabgp_env = ' '.join((
             'exabgp.daemon.user=root',
             'exabgp.log.all=true',
@@ -2202,8 +2202,8 @@ dbs:
         controller = self._get_controller()
         # Ensure exabgp only attempts one connection.
         exabgp_cmd = mininet_test_util.timeout_cmd(
-            'exabgp %s --once -d 2> %s > /dev/null &' % (
-                exabgp_conf_file_name, exabgp_err), 300)
+            'exabgp %s --once -d 2>&1 > %s &' % (
+                exabgp_conf_file_name, exabgp_out), 300)
         exabgp_cli = 'env %s %s' % (exabgp_env, exabgp_cmd)
         controller.cmd(exabgp_cli)
         for _ in range(timeout):
