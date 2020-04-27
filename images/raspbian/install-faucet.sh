0ce39416d9769374c85ca1f5f39726944bc02194
Brad Cowie <brad@wand.net.nz>
2020-04-28 09:04:21 +1200

    Replace gitlab CI with github actions.
     * Upgrade VM image to ubuntu 20.04.
     * Upgrade raspbian image to raspbian buster.

--- images/raspbian/install-faucet.sh
+++ images/raspbian/install-faucet.sh
@@ -4,9 +4,9 @@ echo "Installing Faucet"
 ID="$(grep -Po '(?<=^ID=).*' ${ROOTFS_DIR}/etc/os-release | awk '{print tolower($0)}')"
 CODE="$(grep -Po -m 1 '(?<=\()[^\)]+' ${ROOTFS_DIR}/etc/os-release | awk '{print tolower($0)}')"
 on_chroot << EOF
-apt-get install curl gnupg apt-transport-https lsb-release -y
+apt-get install -y curl apt-transport-https lsb-release tcpdump
 echo "deb https://packagecloud.io/faucetsdn/faucet/${ID}/ ${CODE} main" | sudo tee /etc/apt/sources.list.d/faucet.list
 curl -L https://packagecloud.io/faucetsdn/faucet/gpgkey | sudo apt-key add -
 apt-get update
-apt-get install faucet-all-in-one --fix-missing -y
+apt-get install -y --fix-missing faucet-all-in-one
 EOF
--- images/vm/build-faucet-vm.sh
+++ images/vm/build-faucet-vm.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-export DIB_RELEASE=bionic
+export DIB_RELEASE=focal
 export ELEMENTS_PATH=elements
 
 GIT_ID=$(git describe --tags)
 
-if [ ! -d "/dev/loop0" ]; then
-    echo "Creating a loop device"
-    losetup -D
-    mknod -m 0660 /dev/loop0 b 7 0
-fi
-
 disk-image-create -x -x --checksum -a amd64 -o faucet-amd64-$GIT_ID \
     -t qcow2 \
-    vm ubuntu-minimal cloud-init-nocloud \
-    stable-interface-names dhcp-all-interfaces \
+    vm ubuntu-minimal cloud-init-nocloud dhcp-all-interfaces \
     openssh-server runtime-ssh-host-keys \
     faucet-all-in-one
