264151fcb027a22b303d5997b1a97241d643c972
Brad Cowie <brad@wand.net.nz>
2020-06-10 14:33:09 +1200

    shellcheck

--- docker/install-faucet.sh
+++ docker/install-faucet.sh
@@ -1,25 +1,24 @@
-#!/bin/sh
+#!/bin/ash
 
-set -e
+set -euo pipefail
 
 APK="apk -q"
 BUILDDEPS="gcc python3-dev musl-dev parallel yaml-dev"
 TESTDEPS="bitstring pytest wheel virtualenv"
 PIP3="pip3 -q --no-cache-dir install --upgrade"
-FROOT=/faucet-src
+FROOT="/faucet-src"
 
-dir=`dirname $0`
+dir=$(dirname "$0")
 
 # Clean up
 rm -r "$FROOT/docs"
+${APK} add -U git ${BUILDDEPS}
+"${dir}/retrycmd.sh" "${PIP3} pip"
+"${dir}/retrycmd.sh" "${PIP3} setuptools ${TESTDEPS}"
+"${dir}/retrycmd.sh" "${PIP3} -r ${FROOT}/requirements.txt"
+${PIP3} ${FROOT}
 
-$APK add -U git $BUILDDEPS && \
-  $dir/retrycmd.sh "$PIP3 pip" && \
-  $dir/retrycmd.sh "$PIP3 setuptools $TESTDEPS" && \
-  $dir/retrycmd.sh "$PIP3 -r $FROOT/requirements.txt" && \
-  $PIP3 $FROOT
-
-if [ "$(uname -m)" == "x86_64" ]; then
+if [ "$(uname -m)" = "x86_64" ]; then
   (
   echo "Running unit tests"
   cd "${FROOT}"
@@ -30,9 +29,9 @@ else
   echo "Skipping tests on $(uname -m) platform"
 fi
 
-pip3 uninstall -y $TESTDEPS || exit 1
-for i in $BUILDDEPS ; do
-  $APK del $i || exit 1
+pip3 uninstall -y ${TESTDEPS} || exit 1
+for i in ${BUILDDEPS} ; do
+  ${APK} del "$i" || exit 1
 done
 
 # Smoke test
