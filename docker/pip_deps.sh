b28e4c48352174c9528276426b45efa8f4cbce00
Brad Cowie <brad@wand.net.nz>
2020-11-11 15:03:16 +1300

    Add argument parsing to pip_deps.sh + shellcheck

--- docker/pip_deps.sh
+++ docker/pip_deps.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
-FAUCETHOME=`dirname $0`/..
-FAUCETHOME=`readlink -f $FAUCETHOME`
-PIPARGS="install -q --upgrade $*"
+SCRIPTPATH=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "${SCRIPTPATH}")
+BASEDIR=$(readlink -f "${SCRIPTDIR}/..")
+
+reqs="test-requirements.txt fuzz-requirements.txt adapters/vendors/rabbitmq/requirements.txt"
+pip_args=""
+
+for opt in "$@"; do
+  case "${opt}" in
+    --pip-args=*)
+      pip_args+=" ${opt#*=}"
+      ;;
+    --extra-requirements=*)
+      reqs+=" ${opt#*=}"
+      ;;
+  esac
+done
+
+PIPARGS="install -q --upgrade ${pip_args}"
 
 # Install pip pre-dependencies.
-$FAUCETHOME/docker/retrycmd.sh "pip3 $PIPARGS wheel cython setuptools"
+"${BASEDIR}/docker/retrycmd.sh" "pip3 ${PIPARGS} wheel cython setuptools"
 
-for r in test-requirements.txt fuzz-requirements.txt docs/requirements.txt adapters/vendors/rabbitmq/requirements.txt ; do
-  $FAUCETHOME/docker/retrycmd.sh "pip3 $PIPARGS -r $FAUCETHOME/$r"
+for req in ${reqs}; do
+  "${BASEDIR}/docker/retrycmd.sh" "pip3 ${PIPARGS} -r ${BASEDIR}/${req}"
 done
 
 # Topo unit test needs mininet in user python environment
 if ! python -c 'import mininet.net' 2> /dev/null; then
-  TMPDIR=$(mktemp -d) && pushd $TMPDIR
+  TMPDIR=$(mktemp -d) && pushd "${TMPDIR}"
   git clone https://github.com/mininet/mininet
   cd mininet
   pip3 install -q .
-  popd && rm -rf $TMPDIR
+  popd && rm -rf "${TMPDIR}"
 fi
