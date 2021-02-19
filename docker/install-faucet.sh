7fb55705d6371a15086de12907187ce6146f48dc
Josh Bailey <josh@vandervecken.com>
2021-02-19 04:46:03 +0000

    Use latest PBR, don't deinstall pip (need libs), switch to py3-pip.

--- docker/install-faucet.sh
+++ docker/install-faucet.sh
@@ -10,9 +10,10 @@ FROOT="/faucet-src"
 
 dir=$(dirname "$0")
 
-${APK} add -U git ${BUILDDEPS}
+${APK} update
+${APK} add -U git ${BUILDDEPS} py3-pip
 "${dir}/retrycmd.sh" "${PIP3} pip"
-"${dir}/retrycmd.sh" "${PIP3} setuptools ${TESTDEPS}"
+"${dir}/retrycmd.sh" "${PIP3} --ignore-installed setuptools ${TESTDEPS}"
 "${dir}/retrycmd.sh" "${PIP3} -r ${FROOT}/requirements.txt"
 ${PIP3} ${FROOT}
 
