7e039e05145a32383ead08735b20c6d3ea057706
Brad Cowie <brad@wand.net.nz>
2021-05-07 15:42:42 +1200

    Fix pip feature deprecation (in-tree-build).

--- docker/pip_deps.sh
+++ docker/pip_deps.sh
@@ -30,9 +30,5 @@ done
 
 # Topo unit test needs mininet in user python environment
 if ! python -c 'import mininet.net' 2> /dev/null; then
-  TMPDIR=$(mktemp -d) && pushd "${TMPDIR}"
-  git clone https://github.com/mininet/mininet
-  cd mininet
-  pip3 install -q .
-  popd && rm -rf "${TMPDIR}"
+  pip3 install -q git+https://github.com/mininet/mininet
 fi
