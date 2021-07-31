a147ffee295233c655da8f6ed875f2402851f8cd
Mark Wutzke <mwutzke@users.noreply.github.com>
2021-08-01 07:29:28 +1000

    flake8: Use correct configuration file name
    Now that `pep8` has been removed, a single tool
    is using the configuration file, so rename it to
    a more standard name for `flake8` usage.

--- tests/codecheck/flake8.sh
+++ tests/codecheck/flake8.sh
@@ -6,7 +6,7 @@ SCRIPTPATH=$(readlink -f "$0")
 TESTDIR=$(dirname "${SCRIPTPATH}")
 BASEDIR=$(readlink -f "${TESTDIR}/../..")
 
-config=$(readlink -f "$BASEDIR/.codecheck")
+config=$(readlink -f "$BASEDIR/.flake8")
 srcfiles=$("${TESTDIR}/src_files.sh" "$@" | shuf)
 
 flake8 -j 2 --config "${config}" ${srcfiles}
