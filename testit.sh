8b86f5d023f1c401033a8c62356ccaff19b73406
Josh Bailey <josh@vandervecken.com>
2022-06-16 02:44:19 +0000

    No testit.sh

--- testit.sh
+++
@@ -1,8 +0,0 @@
-sudo docker build --pull -t c65faucet/tests -f Dockerfile.tests .
-sudo docker run --name=faucet-tests \
-	        --sysctl net.ipv6.conf.all.disable_ipv6=0 --privileged --cap-add=ALL --rm \
-		-v /lib/modules:/lib/modules \
-                -v /var/local/lib/docker:/var/lib/docker \
-                -v /tmp/faucet-pip-cache:/var/tmp/pip-cache \
-		-e FAUCET_TESTS="-un FaucetUntaggedTest" \
-                -ti c65faucet/tests
