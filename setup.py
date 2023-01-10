68bb70fb65144fd2e1f1cd157148fd815fecb060
Josh Bailey <josh@vandervecken.com>
2023-01-10 20:46:17 +0000

    remove python3.7

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ from setuptools import setup
 if sys.version_info < (3,):
     print("""You are trying to install faucet on python {py}
 
-Faucet is not compatible with python 2, please upgrade to python 3.7 or newer."""
+Faucet is not compatible with python 2, please upgrade to python 3.8 or newer."""
           .format(py='.'.join([str(v) for v in sys.version_info[:3]])), file=sys.stderr)
     sys.exit(1)
-elif sys.version_info < (3, 7):
+elif sys.version_info < (3, 8):
     print("""You are trying to install faucet on python {py}
 
 Faucet 1.9.0 and above are no longer compatible with older versions of python 3.
@@ -93,7 +93,7 @@ def install_configs():
 setup(
     name='faucet',
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     pbr=True
 )
 
