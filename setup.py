429d3b86d4ca050de0c3a1edba3ab7f040a2fac9
Brad Cowie <brad@faucet.nz>
2025-10-09 10:23:22 +1300

    Remove python 3.9 support

--- setup.py
+++ setup.py
@@ -16,19 +16,19 @@ if sys.version_info < (3,):
     print(
         """You are trying to install faucet on python {py}
 
-Faucet is not compatible with python 2, please upgrade to python 3.9 or newer.""".format(
+Faucet is not compatible with python 2, please upgrade to python 3.10 or newer.""".format(
             py=".".join([str(v) for v in sys.version_info[:3]])
         ),
         file=sys.stderr,
     )
     sys.exit(1)
-elif sys.version_info < (3, 9):
+elif sys.version_info < (3, 10):
     print(
         """You are trying to install faucet on python {py}
 
 Faucet 1.9.0 and above are no longer compatible with older versions of python 3.
 
-Please upgrade to python 3.7 or newer.""".format(
+Please upgrade to python 3.10 or newer.""".format(
             py=".".join([str(v) for v in sys.version_info[:3]])
         )
     )
@@ -104,7 +104,7 @@ def install_configs():
 setup(
     name="faucet",
     setup_requires=["pbr>=1.9", "setuptools>=17.1"],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     pbr=True,
 )
 
